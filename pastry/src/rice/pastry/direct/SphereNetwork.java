1d6ff67d2eefe21b9294e04d58f1e3c614e52a9c
ychu <ychu@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-03-31 04:26:27 +0000

    added SphereNetwork.
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@177 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/pastry/direct/SphereNetwork.java
@@ -0,0 +1,158 @@
+/*************************************************************************
+
+"FreePastry" Peer-to-Peer Application Development Substrate 
+
+Copyright 2002, Rice University. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+- Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+- Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+- Neither  the name  of Rice  University (RICE) nor  the names  of its
+contributors may be  used to endorse or promote  products derived from
+this software without specific prior written permission.
+
+This software is provided by RICE and the contributors on an "as is"
+basis, without any representations or warranties of any kind, express
+or implied including, but not limited to, representations or
+warranties of non-infringement, merchantability or fitness for a
+particular purpose. In no event shall RICE or contributors be liable
+for any direct, indirect, incidental, special, exemplary, or
+consequential damages (including, but not limited to, procurement of
+substitute goods or services; loss of use, data, or profits; or
+business interruption) however caused and on any theory of liability,
+whether in contract, strict liability, or tort (including negligence
+or otherwise) arising in any way out of the use of this software, even
+if advised of the possibility of such damage.
+
+********************************************************************************/
+
+package rice.pastry.direct;
+
+import rice.pastry.*;
+import rice.pastry.messaging.*;
+
+import java.util.*;
+import java.lang.*;
+
+/**
+ * Sphere network topology and idealized node life.
+ *
+ * @version $Id$
+ *
+ * @author Y. Charlie Hu
+ */
+
+public class SphereNetwork implements NetworkSimulator 
+{
+    private Random rng;
+    private HashMap nodeMap;
+    private Vector msgQueue;
+
+    private TestRecord testRecord;
+
+    private class MessageDelivery {
+	private Message msg;
+	private PastryNode node;
+
+	public MessageDelivery(Message m, PastryNode pn) {
+	    msg = m;
+	    node = pn;
+	}
+
+	public void deliver() { 
+	    //System.out.println("delivering to " + node);
+	    //System.out.println(msg);
+		    
+	    node.receiveMessage(msg); 
+	    //System.out.println("----------------------");
+	}
+    }
+
+    private class NodeRecord {
+        public double theta, phi;
+	public boolean alive;
+
+	public NodeRecord() {
+  	    theta = Math.asin(2.0 * rng.nextDouble() - 1.0);
+	    phi   = 2.0 * Math.PI * rng.nextDouble();
+	    
+	    alive = true;
+	}
+
+	public int proximity(NodeRecord nr) {
+	    return (int)(10000 * Math.acos(Math.cos(phi-nr.phi)*Math.cos(theta)*Math.cos(nr.theta) + 
+					   Math.sin(theta)*Math.sin(nr.theta)));
+	}
+    }
+
+    public SphereNetwork() {
+	rng = new Random();
+	nodeMap = new HashMap();
+	msgQueue = new Vector();
+    }
+    
+    public void registerNodeId(NodeId nid)
+    {
+	if (nodeMap.get(nid) != null) throw new Error("collision creating network stats for " + nid);
+	nodeMap.put(nid, new NodeRecord());
+    }
+
+    public boolean isAlive(NodeId nid) {
+	NodeRecord nr = (NodeRecord) nodeMap.get(nid);
+	
+	if (nr == null) throw new Error("asking about node alive for unknown node");
+
+	return nr.alive;
+    }
+
+    public void setAlive(NodeId nid, boolean alive) {
+	NodeRecord nr = (NodeRecord) nodeMap.get(nid);
+	
+	if (nr == null) throw new Error("setting node alive for unknown node");
+
+	nr.alive = alive;
+    }
+
+    public int proximity(NodeId a, NodeId b)
+    {
+	NodeRecord nra = (NodeRecord) nodeMap.get(a);
+	NodeRecord nrb = (NodeRecord) nodeMap.get(b);
+
+	if (nra == null ||
+	    nrb == null) throw new Error("asking about node proximity for unknown node(s)");
+
+	return nra.proximity(nrb);
+    }
+    
+    public void deliverMessage(Message msg, PastryNode node) {
+	MessageDelivery md = new MessageDelivery(msg, node);
+
+	msgQueue.addElement(md);
+    }
+
+    public boolean simulate() {
+	if (msgQueue.size() == 0) return false;
+	
+	MessageDelivery md = (MessageDelivery) msgQueue.firstElement();
+
+	msgQueue.removeElementAt(0);
+
+	md.deliver();
+	
+	return true;
+    }
+    public void setTestRecord( TestRecord tr ){
+	testRecord = tr;
+    }
+    public TestRecord getTestRecord(){
+	return testRecord;
+    }
+}
