ce59da01389b6b18f058dc096f83fcdd0908cbcb
ychu <ychu@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-04-01 03:41:39 +0000

    added javadocs
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@194 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/pastry/direct/TestRecord.java
+++ pastry/src/rice/pastry/direct/TestRecord.java
@@ -40,14 +40,34 @@ public abstract class TestRecord{
     protected int	nNodes;
     protected int 	nTests;
 
+    /**
+     * Constructor.
+     *
+     * @param n number of nodes
+     * @param k number of tests
+     */
+
     public TestRecord( int n, int k ){
 	nNodes = n;
 	nTests = k;
     }
 
+    /**
+     * returns the number of nodes
+     *
+     * @return the number of nodes 
+     */
+    
     public int getNodeNumber( ){
 	return nNodes;
     }
+
+    /**
+     * returns the number of tests
+     *
+     * @return the number of tests
+     */
+    
     public int getTestNumber(){
 	return nTests;
     }
