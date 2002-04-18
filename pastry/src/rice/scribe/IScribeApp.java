ec461cc24240496984aa006d6a60e65ec1ac2227
ssiyer <ssiyer@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-04-18 21:24:01 +0000

    no change
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@251 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/scribe/IScribeApp.java
+++ pastry/src/rice/scribe/IScribeApp.java
@@ -17,29 +17,25 @@ public interface IScribeApp
     /** Called by Scribe on a PUBLISH event when a message arrives to its 
      * destination
      * 
-     * @param msg 
-     * The message sent in the PUBLISH message.
+     * @param msg The message sent in the PUBLISH message.
      */
     public void receiveMessage( ScribeMessage msg );
     
     /** Called by Scribe before the node forwards a message to
      * its children in the multicast tree.
-     * @param msg 
-     * The message about to be forwarded.
+     * @param msg The message about to be forwarded.
      */
     public void forwardHandler( ScribeMessage msg );
     
     /** Invoked by Scribe after a new child is added to one of
      * the node's children tables.
-     * @param msg 
-     * The SUBSCRIBE message from the new child.
+     * @param msg The SUBSCRIBE message from the new child.
      */
     public void subscribeHandler( ScribeMessage msg );
     
     /** Invoked by Scribe just before the "repair" SUBSCRIBE message is sent
      * when a node suspects its parent is faulty.
-     * @param msg 
-     * The SUBSCRIBE message that is sent to repair the multicast tree.
+     * @param msg The SUBSCRIBE message that is sent to repair the multicast tree.
      */
     public void faultHandler( ScribeMessage msg );
     
