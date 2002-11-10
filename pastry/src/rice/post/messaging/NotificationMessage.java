9799b72a273ff99247b15dfa660fc4f2c07cbb0a
willmann <willmann@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-11-10 23:34:32 +0000

    updated notification message to properly inherit from pastry Message class
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@601 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/post/messaging/NotificationMessage.java
+++ pastry/src/rice/post/messaging/NotificationMessage.java
@@ -11,25 +11,60 @@ import java.io.*;
  * message.
  */
 public abstract class NotificationMessage extends Message implements Serializable {
-  private PostClientAddress address = null;
- 
+  private PostClientAddress clientID = null;
+  private PostEntityAddress address = null; 
   /**
    * Constructs a NotificationMessage for the given Email.
    *
-   * @param address The address of the service to which this message
-   *        should be delivered.
+   * @param clientID The address of the service to which this message
+   *	    should be delivered.
+   * @param address The address of the user or group to which this
+   *        message should be delivered. 
    */
-  public NotificationMessage(PostClientAddress address) {
-    this.address = address;
+  public NotificationMessage(PostClientAddress clientID, PostEntityAddress address) {
+   super(address.getAddress());
+   this.clientID = clientID;
+   this.address = address;
   }
   /**
-   * Returns the PostApplicationAddress of the application
+   * Returns the PostEntityAddress of the user or group 
    * to which this noticiation should be delivered.
    *
-   * @return The address of the application to which this should be delivered
+   * @return The address of the user or group to which this should be delivered
    */
-  public PostClientAddress getAddress() {
+  public PostEntityAddress getAddress() {
     return address;
   }
+  /**
+   * Returns the PostClientAddress of the application to 
+   * which this message should be delievered
+   *
+   * @return The address of the service to which this message
+   *         should be delivered 
+   */
+   public PostClientAddress getClientID(){
+     return clientID;
+   }
 
+   /**
+    * Sets the PostEntityAddress to which this message should be delivered
+    *
+    * @param The address of the user or group to which this message should be delivered
+    *
+    */
+    public void setAddress(PostEntityAddress address){
+       this.address = address;
+    }
+  
+   /**
+    * Sets the PostClientAddress to which this message should be delivered
+    *
+    * @param The client address to which this message should be delivered
+    *
+    */
+    public void setClientID(PostClientAddress clientID){
+      this.clientID = clientID;
+    }
+
+    
 }
