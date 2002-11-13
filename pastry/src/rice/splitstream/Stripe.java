63009a3188f07ec4b168696dcaad2214f55117de
abpost <abpost@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-11-13 04:00:26 +0000

    *** empty log message ***
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@633 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/splitstream/Stripe.java
+++ pastry/src/rice/splitstream/Stripe.java
@@ -5,27 +5,33 @@ import java.util.Observable;
 /**
  * This class encapsulates all data about an individual stripe.
  */
-public class Stripe extends Observable{
+public class Stripe extends Observable implements IScribe{
+    private int stripeState = 0;
+    private stripeId = null;
+    private Channel channel =  null;
+    private InputStream inputStream = null;
+    private OutputStream outputStream = null;
+
    /**
     * gets the StripeID for this stripe
     * @return theStripeID 
     */
-   public StripeId getStripeID(){return null;}
+   public StripeId getStripeId(){return stripeId;}
    /**
     * gets the Channel that this stripe is a part of 
     * @return Channel Object 
     */
-   public Channel getChannel(){return null;}
+   public Channel getChannel(){return channel;}
     /**
      * Gets the OutputStream for this stripe.
      * @return StripeOutputStream for this stripe
      */
-    public OutputStream getOutputStream(){return null;}
+    public OutputStream getOutputStream(){return outputStream;}
     /**
      * Gets the InputStream for this Stripe.
      * @return StripeInputStream for this Stripe
      */
-     public InputStream getInputStream(){return null;}
+     public InputStream getInputStream(){return inputStream;}
      /**
       * Leaves this stripe
       */
@@ -34,7 +40,7 @@ public class Stripe extends Observable{
     * get the state of the Stripe 
     * @return int s
     */
-    public int getState(){ return 0;}
+    public int getState(){ return stripeState;}
 
     public static final int STRIPE_VALID = 0;
     public static final int STRIPE_INVALID = 1;
