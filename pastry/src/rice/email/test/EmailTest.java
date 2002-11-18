372fed4293340fd8b689343f427ddb91898a7f20
druths <druths@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-11-18 04:47:17 +0000

    Added core test files
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@680 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/email/test/EmailTest.java
@@ -0,0 +1,65 @@
+package rice.email.test;
+
+import rice.pastry.standard.*;
+import rice.pastry.*;
+import rice.pastry.wire.*;
+import rice.post.*;
+import rice.email.*;
+import rice.past.*;
+import rice.scribe.*;
+import rice.storage.*;
+
+import java.security.*;
+
+/**
+ * This class provides helper functions for all email-testing
+ * code.
+ */
+public class EmailTest {
+
+	protected EmailService createEmailService() {
+
+		try {
+
+		RandomNodeIdFactory rnd = new RandomNodeIdFactory();
+		int port = (int)(((double) 16000)*Math.random());
+
+		WirePastryNodeFactory idFactory = new WirePastryNodeFactory(rnd, port);
+		PastryNode localNode = idFactory.newNode(null);
+		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+		KeyPair caPair = kpg.generateKeyPair();
+		KeyPair pair = kpg.generateKeyPair();
+
+		PostUserAddress address = new PostUserAddress("test");
+
+		NodeId nodeId = localNode.getNodeId();
+
+		StorageManager storage = new MemoryStorageManager();
+
+		Scribe scribe = new Scribe(localNode, null);
+		PASTService past = new PASTServiceImpl(localNode, storage);
+		Post post = new Post(localNode, past, scribe, address, pair, null, caPair.getPublic());
+		EmailService email = new EmailService(post);
+
+		return email;
+		} catch(PostException pe) {
+			pe.printStackTrace();
+			return null;
+		} catch(NoSuchAlgorithmException nsae) {
+			nsae.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		EmailTest et = new EmailTest();
+
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Creating " + i);
+			et.createEmailService();
+		}
+
+		Thread.sleep(5000);
+	}
+}
