38151f1c93686936c8d2b708d4ef3e9e295b4370
creis <creis@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-11-19 08:06:45 +0000

    Wrote the new command-based interface for PASTService, but moved it out of the way for the time being so the old PAST will still compile.
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@695 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/past/PASTService.java
+++ pastry/src/rice/past/PASTService.java
@@ -1,85 +1,148 @@
 package rice.past;
 
+import java.io.Serializable;
+
+import rice.pastry.NodeId;
+import rice.pastry.security.Credentials;
+import rice.storage.StorageObject;
+
+// temporary
 import rice.storage.StorageManager;
+
 
 /**
  * @(#) PASTService.java
  *
  * This interface is exported by PAST for any applications or components
  * which need to store replicated copies of documents on the Pastry
- * network.  It provides the same interface as the local StorageManager.
+ * network.
  *
  * @version $Id$
  * @author Charles Reis
  */
-public interface PASTService {
+public interface PASTService extends StorageManager {
+  
+}
+
+
+/**
+ * This is the new event-driven interface for the PAST service.
+ * All methods are asynchronous, and receive their results using
+ * the command pattern.
+ */
+interface NewPASTService {
+  
   /**
    * Inserts an object with the given ID into distributed storage.
-   * Asynchronously returns a Boolean as the result to the provided
-   * ReceiveResultCommand, indicating whether the insert was successful.
+   * Asynchronously returns a boolean as the result to the provided
+   * InsertResultCommand, indicating whether the insert was successful.
    * 
    * @param id Pastry key identifying the object to be stored
    * @param obj Persistable object to be stored
    * @param authorCred Author's credentials
    * @param command Command to be performed when the result is received
-   * @return unique identifier allowing application to recognize the result
-   * of this method call
    */
-  public ResultIdentifier insert(NodeId id, Serializable obj, 
-                                 Credentials authorCred,
-                                 ReceiveResultCommand command);
+  public void insert(NodeId id, Serializable obj, Credentials authorCred,
+                     InsertResultCommand command);
   
   /**
    * Stores an update to the object with the given ID.
-   * Asynchronously returns a Boolean as the result to the provided
-   * ReceiveResultCommand, indicating whether the insert was successful.
+   * Asynchronously returns a boolean as the result to the provided
+   * UpdateResultCommand, indicating whether the insert was successful.
    * 
    * @param id Pastry key of original object to be updated
    * @param update Persistable update to the original object
    * @param authorCred Update Author's credentials
    * @param command Command to be performed when the result is received
-   * @return unique identifier allowing application to recognize the result
-   * of this method call
    */
-  public ResultIdentifier update(NodeId id, Serializable update, 
-                                 Credentials authorCred, 
-                                 ReceiveResultCommand command);
+  public void update(NodeId id, Serializable update, Credentials authorCred, 
+                     UpdateResultCommand command);
   
   /**
    * Retrieves the object and all associated updates with the given ID.
    * Asynchronously returns a StorageObject as the result to the provided
-   * ReceiveResultCommand.
+   * LookupResultCommand.
    * 
    * @param id Pastry key of original object
    * @param command Command to be performed when the result is received
-   * @return unique identifier allowing application to recognize the result
-   * of this method call
    */
-  public ResultIdentifier lookup(NodeId id, ReceiveResultCommand command);
+  public void lookup(NodeId id, LookupResultCommand command);
   
   /**
    * Determines whether an object is currently stored at the given ID.
-   * Asynchronously returns a Boolean as the result to the provided
-   * ReceiveResultCommand, indicating whether the object exists.
+   * Asynchronously returns a boolean as the result to the provided
+   * ExistsResultCommand, indicating whether the object exists.
    * 
    * @param id Pastry key of original object
    * @param command Command to be performed when the result is received
-   * @return unique identifier allowing application to recognize the result
-   * of this method call
    */
-  public ResultIdentifier exists(NodeId id, ReceiveResultCommand command);
+  public void exists(NodeId id, ExistsResultCommand command);
   
   /**
    * Reclaims the storage used by the object with the given ID.
-   * Asynchronously returns a Boolean as the result to the provided
-   * ReceiveResultCommand, indicating whether the delete was successful.
+   * Asynchronously returns a boolean as the result to the provided
+   * DeleteResultCommand, indicating whether the delete was successful.
    * 
    * @param id Pastry key of original object
    * @param authorCred Author's credentials
-   * @return unique identifier allowing application to recognize the result
-   * of this method call
    */
-  public ResultIdentifier delete(NodeId id, Credentials authorCred,
-                                 ReceiveResultCommand command);
+  public void delete(NodeId id, Credentials authorCred,
+                     DeleteResultCommand command);
   
+}
+
+
+/**
+ * Command to perform after an insert is completed.
+ */
+interface InsertResultCommand {
+  /**
+   * Called when a call to insert completes.
+   * @param success Whether the insert was successful
+   */
+  public void receiveInsertResult(boolean success);
+}
+
+/**
+ * Command to perform after an update is completed.
+ */
+interface UpdateResultCommand {
+  /**
+   * Called when a call to update completes.
+   * @param success Whether the update was successful
+   */
+  public void receiveUpdateResult(boolean success);
+}
+
+/**
+ * Command to perform after a lookup is completed.
+ */
+interface LookupResultCommand {
+  /**
+   * Called when a call to lookup completes.
+   * @param result The object stored at the requested address
+   */
+  public void receiveLookupResult(StorageObject result);
+}
+
+/**
+ * Command to perform after an existence check is completed.
+ */
+interface ExistsResultCommand {
+  /**
+   * Called when a call to exists completes.
+   * @param success Whether the desired object exists
+   */
+  public void receiveExistsResult(boolean exists);
+}
+
+/**
+ * Command to perform after a delete is completed.
+ */
+interface DeleteResultCommand {
+  /**
+   * Called when a call to delete completes.
+   * @param success Whether the delete was successful
+   */
+  public void receiveDeleteResult(boolean successful);
 }
