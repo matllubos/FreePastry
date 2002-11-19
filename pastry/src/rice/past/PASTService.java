db3b1a204fe2273cdd552ce39517ef3822fd5d6d
creis <creis@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-11-19 05:58:16 +0000

    Modified the PASTService interface to support the new event-driven architecture.  (PASTServiceImpl has not yet been updated.)
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@694 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/past/PASTService.java
+++ pastry/src/rice/past/PASTService.java
@@ -12,5 +12,74 @@ import rice.storage.StorageManager;
  * @version $Id$
  * @author Charles Reis
  */
-public interface PASTService extends StorageManager {
+public interface PASTService {
+  /**
+   * Inserts an object with the given ID into distributed storage.
+   * Asynchronously returns a Boolean as the result to the provided
+   * ReceiveResultCommand, indicating whether the insert was successful.
+   * 
+   * @param id Pastry key identifying the object to be stored
+   * @param obj Persistable object to be stored
+   * @param authorCred Author's credentials
+   * @param command Command to be performed when the result is received
+   * @return unique identifier allowing application to recognize the result
+   * of this method call
+   */
+  public ResultIdentifier insert(NodeId id, Serializable obj, 
+                                 Credentials authorCred,
+                                 ReceiveResultCommand command);
+  
+  /**
+   * Stores an update to the object with the given ID.
+   * Asynchronously returns a Boolean as the result to the provided
+   * ReceiveResultCommand, indicating whether the insert was successful.
+   * 
+   * @param id Pastry key of original object to be updated
+   * @param update Persistable update to the original object
+   * @param authorCred Update Author's credentials
+   * @param command Command to be performed when the result is received
+   * @return unique identifier allowing application to recognize the result
+   * of this method call
+   */
+  public ResultIdentifier update(NodeId id, Serializable update, 
+                                 Credentials authorCred, 
+                                 ReceiveResultCommand command);
+  
+  /**
+   * Retrieves the object and all associated updates with the given ID.
+   * Asynchronously returns a StorageObject as the result to the provided
+   * ReceiveResultCommand.
+   * 
+   * @param id Pastry key of original object
+   * @param command Command to be performed when the result is received
+   * @return unique identifier allowing application to recognize the result
+   * of this method call
+   */
+  public ResultIdentifier lookup(NodeId id, ReceiveResultCommand command);
+  
+  /**
+   * Determines whether an object is currently stored at the given ID.
+   * Asynchronously returns a Boolean as the result to the provided
+   * ReceiveResultCommand, indicating whether the object exists.
+   * 
+   * @param id Pastry key of original object
+   * @param command Command to be performed when the result is received
+   * @return unique identifier allowing application to recognize the result
+   * of this method call
+   */
+  public ResultIdentifier exists(NodeId id, ReceiveResultCommand command);
+  
+  /**
+   * Reclaims the storage used by the object with the given ID.
+   * Asynchronously returns a Boolean as the result to the provided
+   * ReceiveResultCommand, indicating whether the delete was successful.
+   * 
+   * @param id Pastry key of original object
+   * @param authorCred Author's credentials
+   * @return unique identifier allowing application to recognize the result
+   * of this method call
+   */
+  public ResultIdentifier delete(NodeId id, Credentials authorCred,
+                                 ReceiveResultCommand command);
+  
 }
