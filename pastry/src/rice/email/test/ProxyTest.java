c9abb5b07cfcec1d12eba8e621e2788936f54685
dwp <dwp@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-11-25 19:17:53 +0000

    Proxytest stuff
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@791 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/email/test/ProxyTest.java
+++ pastry/src/rice/email/test/ProxyTest.java
@@ -1,32 +1,65 @@
 package rice.email.test;
 
 import java.net.InetAddress;
+import java.io.*;
+import java.util.*;
 
 import rice.email.*;
+import rice.email.messaging.*;
 import rice.email.proxy.*;
 
 /**
  * Test class for dealing with proxies, ensuring that they work
  * and make the appropriate connections.
  */
+
+class EmailReceiver implements Observer {
+    public void update(Observable src, Object args) {
+	System.out.println("SRC: " + src + ", ARG: " + args);
+	
+	if(args instanceof EmailNotificationMessage) {
+	    System.out.println("Email received");
+	} else {
+	    System.out.println("Not an email");
+	}
+    }
+}
+
 public class ProxyTest {
 
     public static void main(String[] args) throws Exception {
 
 	EmailTest et = new EmailTest();
-	EmailService eserv = et.createEmailService();
+	String[] nameset = new String[2];
+	nameset[0] = "<dwp@rice.edu>";
+	nameset[1] = "<recipient@recipient.org>";
+	EmailService[] eservset = et.createEmailServices(nameset, 2000);
+	EmailService sender = eservset[0];
+	EmailService receiver = eservset[1];
 
+	Thread.sleep(5000);
+	
 	IMAPProxy imp = new
 	    IMAPProxy(InetAddress.getByName("imap.owlnet.rice.edu"),
 		      143);
 
-	imp.attach(eserv, "dwp", args[0]);
+	BufferedReader r = new BufferedReader(new
+	    FileReader("password"));
+	String pass = r.readLine();
 
+	imp.attach(receiver, "dwp", pass);
+ 
+	System.err.println("IMAP proxy started.");
+
+	
+	
 	SMTPProxy smp = new
 	    SMTPProxy(InetAddress.getByName("localhost"),
 		      11235);
 
-	smp.attach(eserv);
+	smp.attach(sender);
+
+	System.err.println("SMTP proxy started.");
 
 	while(true) {
 	    Thread.sleep(5000);
@@ -34,3 +67,5 @@ public class ProxyTest {
     }	
 
 }
+
+
