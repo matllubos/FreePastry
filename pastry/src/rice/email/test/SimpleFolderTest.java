da88c2de6ff853eb29d090d4a0e7c5fdf7bd4a50
montgom <montgom@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-11-25 06:38:14 +0000

    *** empty log message ***
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@776 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/email/test/SimpleFolderTest.java
+++ pastry/src/rice/email/test/SimpleFolderTest.java
@@ -8,6 +8,8 @@ import java.util.*;
 public class SimpleFolderTest extends EmailTest {
   public static final int START = 0;
   public static final int GOT_ROOT = 10;
+  public static final int MADE_CHILD_1 = 20;
+  public static final int MADE_CHILD_2 = 30;
 
   // the sender
   private EmailService _sender;
@@ -15,6 +17,9 @@ public class SimpleFolderTest extends EmailTest {
   private Folder _rootFolder;
   // the current state of the program
   private int _state;
+
+  // the immediate subFolders
+  Folder _subFolder1, _subFolder2;
 
   // methods
   public void startTest() {
@@ -27,50 +32,56 @@ public class SimpleFolderTest extends EmailTest {
     if (_state == START) {
       // Create the sender
       EmailService[] services;
-      services = this.createEmailServices(new String[]{"user1"});		    
+      services = this.createEmailServices(new String[]{"user1"}, 2000);		    
       _sender = services[0];
       if (_sender == null) {
 	System.out.println("Could not create an EmailService to work with; Bailing.");
-	exit(1);
+	return;
       }
       // get the root
       _state = GOT_ROOT;
-      Continuation command = new GetFolderCont(_rootFolder);      
+      Continuation command = new SimpleFolderCont(_rootFolder);      
       _sender.getRootFolder(command);
     }
     else if (_state == GOT_ROOT) {
+      // once we have the name of the root, print it out
       System.out.println("Name of the root Folder: " + _rootFolder.getName());
+
+      // create a new Folder under the root
+      _state = MADE_CHILD_1;
+      Continuation command = new SimpleFolderCont(_subFolder1);
+      _rootFolder.createChildFolder("Adam", command);
+    }
+    else if (_state == MADE_CHILD_1) {
+      // print out the info for the child folders
+      System.out.println("Reached make child 1");
+      String[] childNames = _rootFolder.getChildren();
+      System.out.println("Number of child folders is " + childNames.length);
+      for (int i = 0; i < childNames.length; i++) {
+	System.out.println(childNames[i]);
+      }
+      // create a new Folder under the root
+      _state = MADE_CHILD_2;
+      Continuation command = new SimpleFolderCont(_subFolder2);
+      _rootFolder.createChildFolder("Bob", command);
+    }
+    else if (_state == MADE_CHILD_2) {
+      System.out.println("Reached make child 1");
+      // print out the info for the child folders
+      System.out.println("Reached make child 1");
+      String[] childNames = _rootFolder.getChildren();
+      System.out.println("Number of child folders is " + childNames.length);
+      for (int i = 0; i < childNames.length; i++) {
+	System.out.println(childNames[i]);
+      }
+      // do the next step
     }
   }
 
-
-    /*
-      // create an email to send
-      String subject = "Hello World";
-      String bodyText = "Hello World!";
-		
-      EmailData body = new EmailData(bodyText.getBytes());
-      EmailData[] attachments = new EmailData[0];
-
-      PostEntityAddress[] recipients = new PostEntityAddress[1];
-      recipients[0] = receiver.getPost().getEntityAddress();
-
-      Email email = new Email((PostUserAddress) sender.getPost().getEntityAddress(),
-      recipients, subject,
-      body, attachments);
-
-      try {
-      sender.sendMessage(email, null);
-      } catch(Exception e) {
-      System.err.println("Error sending email");
-      e.printStackTrace();
-      }
-    */
-
   // Gets the root Folder, continues on with test
-  protected class GetFolderCont implements Continuation {
+  protected class SimpleFolderCont implements Continuation {
     Folder _folder;
-    public GetFolderCont(Folder folder) {
+    public SimpleFolderCont(Folder folder) {
       _folder = folder;      
     }
     public void start() {}
