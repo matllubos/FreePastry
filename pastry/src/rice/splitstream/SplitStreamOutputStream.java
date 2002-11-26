8aad7bd868431298e2621c5b766304cd1affdaed
abpost <abpost@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-11-26 02:55:18 +0000

    Added
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@803 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/splitstream/SplitStreamOutputStream.java
@@ -0,0 +1,31 @@
+package rice.splitstream;
+import rice.splitstream.messaging.*;
+import java.io.*;
+import rice.pastry.security.*;
+import rice.scribe.*;
+public class SplitStreamOutputStream extends ByteArrayOutputStream{
+
+   private StripeId stripeId;
+   private IScribe scribe;
+   private Credentials cred;
+ 
+   public SplitStreamOutputStream(StripeId stripeId, IScribe scribe, Credentials cred){
+	super();
+	this.stripeId = stripeId;
+	this.scribe = scribe;
+	this.cred = cred;
+   }
+   public void write(byte[] b, int off, int len){
+	super.write(b, off, len);
+	/* Do splitstream stuff */
+	scribe.multicast(stripeId, b, cred);
+   }
+   public void write(int b){
+	super.write(b); 
+	System.out.println("Attempting to Send Data");
+	//if(scribe.multicast(stripeId, b, cred))
+	System.out.println("Attempting to Send Data");
+	/* Do splitstream stuff */
+   } 
+
+}
