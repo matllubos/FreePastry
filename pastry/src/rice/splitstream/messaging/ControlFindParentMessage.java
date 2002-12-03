613f74fdb4d95652124caa4647eed43159c3616b
atuls <atuls@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-12-03 06:14:17 +0000

    fixed a annoying bug
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@848 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/splitstream/messaging/ControlFindParentMessage.java
+++ pastry/src/rice/splitstream/messaging/ControlFindParentMessage.java
@@ -19,25 +19,27 @@ public class ControlFindParentMessage extends Message implements Serializable
 {
     Vector send_to;
     Vector already_seen;
-    StripeId stripe_id;
+    StripeId stripe_id; // the stripe which we are looking for
     Stripe recv_stripe = null;
     ChannelId channel_id;
     NodeHandle originalSource ;
     final int DEFAULT_CHILDREN = 20;
 
+    // topicId is the spare capacity tree Id.
+    // stripeId is the stripe id for which local node is interested in .
     public ControlFindParentMessage( Address addr, NodeHandle source, NodeId topicId, Credentials c, StripeId stripe_id, ChannelId channel_id)
     {
-       super( addr );
-       send_to = new Vector();
-       already_seen = new Vector();
-       this.stripe_id = stripe_id;
-       this.channel_id = channel_id;
-       this.originalSource = source;
+	super( addr );
+	send_to = new Vector();
+	already_seen = new Vector();
+	this.stripe_id = stripe_id;
+	this.channel_id = channel_id;
+	this.originalSource = source;
     }
 
     public StripeId getStripeId()
     {
-       return stripe_id;
+	return stripe_id;
     }
 
     /**
@@ -50,14 +52,21 @@ public class ControlFindParentMessage extends Message implements Serializable
     private boolean isInRootPath( IScribe scribe, NodeHandle source )
     {
         if ( recv_stripe != null )
-        {
-            //return recv_stripe.getRootPath().contains( source );
-		return false;
-        }
+	    {
+		if ( recv_stripe.getRootPath() != null )
+		    {
+			return recv_stripe.getRootPath().contains( source );
+		    }
+		else
+		    {
+			//receiving node has been dropped; don't attach
+			return true;
+		    }
+	    }
         else
-        {
-            return false;
-        }
+	    {
+		return false;
+	    }
     }
 
     /**
@@ -69,9 +78,9 @@ public class ControlFindParentMessage extends Message implements Serializable
         int total = 0;
 
         for (int i=0; i<topics.size(); i++)
-        {
-            total += scribe.numChildren( ((Topic)topics.get(i)).getTopicId() );
-        }
+	    {
+		total += scribe.numChildren( ((Topic)topics.get(i)).getTopicId() );
+	    }
 
         return total;
     }
@@ -83,101 +92,112 @@ public class ControlFindParentMessage extends Message implements Serializable
      * @param scribe The SplitStream application
      * @param topic The stripe that this message is relevant to
      */
-    public void handleMessage( Scribe scribe, Topic topic, Channel channel )
+    public boolean handleMessage( Scribe scribe, Topic topic, Channel channel, Stripe stripe )
     {
-        //System.out.println("Forwarding at " + scribe.getNodeId());
+	recv_stripe = stripe;
+        //System.out.println("Forwarding at " + scribe.getNodeId()+" for stipe "+stripe.getStripeId());
         Credentials c = new PermissiveCredentials();
-        if ( topic == null )
-        {
-           if ( send_to.size() != 0 )
-           {
-              already_seen.add( 0, send_to.remove(0) );
-           }
-           else
-           {
-              already_seen.add( 0, scribe.getNodeHandle() );
-           }
-           if ( send_to.size() != 0 )
-           {
-              channel.routeMsgDirect( (NodeHandle) send_to.get(0), this, c, null );
-           }
-           else
-           {
-              channel.routeMsgDirect( originalSource, this, c, null );
-           }
-        }
-        else
-        {
-        if ( send_to.size() != 0 )
-        {
-            already_seen.add( 0, send_to.remove(0) );
-        }
-        else
-        {
-            already_seen.add( 0, scribe.getNodeHandle() );
-        }
-        BandwidthManager bandwidthManager = channel.getBandwidthManager();
+	Topic stripeTopic = scribe.getTopic(recv_stripe.getStripeId());
+        if ( stripeTopic == null )
+	    {
+		if ( send_to.size() != 0 )
+		    {
+			already_seen.add( 0, send_to.remove(0) );
+		    }
+		else
+		    {
+			already_seen.add( 0, scribe.getNodeHandle() );
+		    }
+		if ( send_to.size() != 0 )
+		    {
+			channel.routeMsgDirect( (NodeHandle) send_to.get(0), this, c, null );
+		    }
+		else
+		    {
+			channel.routeMsgDirect( originalSource, this, c, null );
+		    }
 
-        if ( ( bandwidthManager.canTakeChild( channel ) ) &&
-             ( !isInRootPath( scribe, originalSource ) ) &&
-             ( originalSource != scribe.getLocalHandle()) )
-        {
-           //System.out.println("TAKING ON CHILD "  + originalSource.getNodeId());scribe.addChild( originalSource, stripe_id );
-	   channel.stripeSubscriberAdded();
-           channel.routeMsgDirect( originalSource,
-                                  new ControlFindParentResponseMessage( channel.getAddress(),
-                                                                        scribe.getNodeHandle(),
-                                                                        channel_id,
-                                                                        c,
-                                                                        new Boolean( true ), stripe_id ),
-                                  c,
-                                  null );
-           int default_children;
-
-           if ( !bandwidthManager.canTakeChild( channel ) )
-           {
-               //scribe.leave( topic.getTopicId(), null, c );
-           }
-
-        }
+	    }
         else
-        {   if(topic == null) System.out.println("TOPIC IS NULL");
-            Vector v = scribe.getChildren( topic.getTopicId() );
-            //System.out.println( "Children of node "+ scribe.getNodeId() + " are " + v );
-            if ( v != null )
-            {
-                send_to.addAll( 0, scribe.getChildren( topic.getTopicId() ) );
-            }
-            while ( ( send_to.size() > 0 ) &&
-                    ( already_seen.contains( send_to.get(0) ) ) )
-            {
-                send_to.remove( 0 );
-            }
-            if ( send_to.size() > 0 )
-            {
-                channel.routeMsgDirect( (NodeHandle)send_to.get(0), this, c, null );
-            }
-            else
-            {
-                if ( !scribe.isRoot( topic.getTopicId() ) )
-                {
-                    channel.routeMsgDirect( scribe.getParent( topic.getTopicId() ), this, c, null );
-                }
-                else
-                {
-                    System.out.println( "You're screwed, we're at the root" );
-                    channel.routeMsgDirect( originalSource,
-                                           new ControlFindParentResponseMessage( channel.getAddress(),
-                                                                                 scribe.getNodeHandle(),
-                                                                                 channel_id,
-                                                                                 c,
-                                                                                 new Boolean( false ), stripe_id ),
-                                           c,
-                                           null );
-                }
-            }
-        }
-        }
+	    {
+		if ( send_to.size() != 0 )
+		    {
+			already_seen.add( 0, send_to.remove(0) );
+		    }
+		else
+		    {
+			already_seen.add( 0, scribe.getNodeHandle() );
+		    }
+		BandwidthManager bandwidthManager = channel.getBandwidthManager();
+
+		if ( ( bandwidthManager.canTakeChild( channel ) ) &&
+		     ( !isInRootPath( scribe, originalSource ) ) &&
+		     ( originalSource != scribe.getLocalHandle()) )
+		    {
+			//System.out.println("TAKING ON CHILD "  + originalSource.getNodeId());
+			scribe.addChild( originalSource, recv_stripe.getStripeId() );
+			//System.out.println("List of children for node "+scribe.getNodeId()+ " for topicid "+stripe_id+" recv_strpie.getStripe "+recv_stripe.getStripeId());
+			//Vector vc = scribe.getChildren((NodeId)recv_stripe.getStripeId() );
+			//for(int i = 0; i < vc.size(); i++)
+			//   System.out.println("**"+((NodeHandle)vc.elementAt(i)).getNodeId()+" **");
+			channel.stripeSubscriberAdded();
+			channel.routeMsgDirect( originalSource,
+						new ControlFindParentResponseMessage( channel.getAddress(),
+										      scribe.getNodeHandle(),
+										      channel_id,
+										      c,
+										      new Boolean( true ), stripe_id ),
+						c,
+						null );
+			int default_children;
+
+			if ( !bandwidthManager.canTakeChild( channel ) )
+			    {
+				//scribe.leave( topic.getTopicId(), null, c );
+			    }
+	   
+			//System.out.println("Node "+scribe.getNodeId()+ " taking the child "+originalSource.getNodeId());
+
+		    }
+		else
+		    {   if(topic == null) System.out.println("TOPIC IS NULL");
+		    Vector v = scribe.getChildren( topic.getTopicId() );
+		    //System.out.println( "Children of node "+ scribe.getNodeId() + " are " + v );
+		    if ( v != null )
+			{
+			    send_to.addAll( 0, scribe.getChildren( topic.getTopicId() ) );
+			}
+		    while ( ( send_to.size() > 0 ) &&
+			    ( already_seen.contains( send_to.get(0) ) ) )
+			{
+			    send_to.remove( 0 );
+			}
+		    if ( send_to.size() > 0 )
+			{
+			    channel.routeMsgDirect( (NodeHandle)send_to.get(0), this, c, null );
+			}
+		    else
+			{
+			    if ( !scribe.isRoot( topic.getTopicId() ) )
+				{
+				    channel.routeMsgDirect( scribe.getParent( topic.getTopicId() ), this, c, null );
+				}
+			    else
+				{
+				    System.out.println( "You're screwed, we're at the root" );
+				    channel.routeMsgDirect( originalSource,
+							    new ControlFindParentResponseMessage( channel.getAddress(),
+												  scribe.getNodeHandle(),
+												  channel_id,
+												  c,
+												  new Boolean( false ), stripe_id ),
+							    c,
+							    null );
+				}
+			}
+		    }
+	    }
+	return false;
     }
 
     public String toString()
