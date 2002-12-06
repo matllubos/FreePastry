a495300239f9b3914817d8b7d85dae1f4b1e0ce3
abpost <abpost@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2002-12-06 04:49:19 +0000

    did some more documentation
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@882 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/splitstream/SplitStreamImpl.java
+++ pastry/src/rice/splitstream/SplitStreamImpl.java
@@ -12,14 +12,19 @@ import rice.pastry.messaging.*;
 import rice.pastry.security.*;
 
 import rice.splitstream.messaging.*;
-/*
- * @(#) SplitStreamImpl.java
- *
- * @author Ansley Post
- */
 
 /**
- * This is the implementing class of the SplitStream service 
+ * This is the implementing class of the ISplitStream interface. It 
+ * provides the functionality of creating and attaching to channels.
+ * It also provides alot of implementation details. It handles the
+ * creation of Channel objects in the path of the Channel tree. It 
+ * also monitors the creation of stripes interior to the tree and
+ * keeps track of the bandwidth used until the user subscribes to 
+ * the channel. It implements the IScribeApp interface for this reason
+ *
+ * @(#) SplitStreamImpl.java
+ * @version $Id$
+ * @author Ansley Post
  */
 public class SplitStreamImpl implements ISplitStream, IScribeApp,IScribeObserver{
     /**
@@ -53,7 +58,10 @@ public class SplitStreamImpl implements ISplitStream, IScribeApp,IScribeObserver
      * which is ready when Scribe is ready
      */
     private Set m_apps = new HashSet();
-
+    
+    /**
+     * Flag for whether this obj is ready
+     */
     private boolean m_ready = false;
 
     /**
@@ -112,35 +120,71 @@ public class SplitStreamImpl implements ISplitStream, IScribeApp,IScribeObserver
 	return channel;
    }
 
+   /**
+    * Gets the bandwidth manager associated with this splitstream object
+    * 
+    * @return BandwidthManager that is associated with this splitstream
+    */ 
    public BandwidthManager getBandwidthManager(){
       return bandwidthManager;
    }
 
+   /**
+    * Sets the bandwidthManager for this splitstream
+    *
+    * @param bandwidthManager the new bandwidthManager
+    */
    public void setBandwidthManager(BandwidthManager bandwidthManager){
        this.bandwidthManager = bandwidthManager;
    }
 
    /** - IScribeObserver Implementation -- */
-   public void update(Object topicId){
-
+   /**
+    * The update method called when a new topic is created at this
+    * node.  When a new topic is created we join it so we can receive
+    * any more messages that come for it.
+    *
+    * @param topicId the new topic being created
+    */
+    public void update(Object topicId){
 	if(scribe.join((NodeId) topicId, this, credentials)){}
    } 
 
    /** - IScribeApp Implementation -- */
-   public void faultHandler(ScribeMessage msg, NodeHandle faultyParent){}
+   /**
+    * The method called when a fault occurs. Currently not implemented.
+    *
+    * @param msg The message to be sent on the failure
+    * @param faultyParent the node that caused the fault 
+    */
+    public void faultHandler(ScribeMessage msg, NodeHandle faultyParent){}
    public void forwardHandler(ScribeMessage msg){}
    public void receiveMessage(ScribeMessage msg){
      /* Check the type of message */
      /* then make call accordingly */
      //System.out.println("Recieved message");
    }
-   
+  
+   /**
+    * Upcall generated when the underlying scribe is ready
+    */ 
    public void scribeIsReady(){
        //System.out.println("Scribe is Ready");
        m_ready = true;
        notifyApps();
    }
-   
+
+   /**
+    * The upcall generated when a subscribe message is recieved
+    * This currently handles the  implicit creation of channel objects
+    * when they don't exist at this node
+    *
+    * @param topicId the topic being subscribed/dropped
+    * @param child the child to be added/dropped
+    * @param was added wether the was a subscribe or an unsubscribe
+    * @param data the date that was in the subscribe/unsubscribe message
+    * 
+    */
    public void subscribeHandler(NodeId topicId, 
                                NodeHandle child, 
                                boolean wasAdded,  
@@ -172,11 +216,24 @@ public class SplitStreamImpl implements ISplitStream, IScribeApp,IScribeObserver
     public IScribe getScribe(){
 	return scribe;
     }
-
+    
+    /**
+     * registers an app to be notified when a fault occurs
+     * that is so severe that the application can not handle it
+     * by automatically repairing it. For example if there is
+     * no spare capacity left in the system and node is looking
+     * for a new parent
+     * 
+     * @param app the app to be registered
     public void registerApp(ISplitStreamApp app){
 	m_apps.add(app);
     }
-
+   
+    /**
+     * called when the apps registered with this splitstream
+     * object must be notified of something
+     *
+     */
     public void notifyApps(){
 	Iterator it = m_apps.iterator();
 	
