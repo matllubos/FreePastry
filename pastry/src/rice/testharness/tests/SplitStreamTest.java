5b3222723cc83c3b800e3868650648978ce9ca43
atuls <atuls@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2003-10-18 00:06:45 +0000

    Modified the tests to work on current Scribe and SplitStream.
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1429 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/testharness/tests/SplitStreamTest.java
+++ pastry/src/rice/testharness/tests/SplitStreamTest.java
@@ -49,11 +49,11 @@ import rice.pastry.routing.*;
 import rice.pastry.leafset.*;
 import rice.pastry.dist.*;
 
-import rice.scribe.*;
-import rice.scribe.maintenance.*;
+import rice.p2p.scribe.*;
+import rice.p2p.scribe.messaging.*;
+import rice.p2p.commonapi.Node;
 
-import rice.splitstream.*;
-import rice.splitstream.messaging.*;
+import rice.p2p.splitstream.*;
 
 import java.util.*;
 import java.io.*;
@@ -70,340 +70,342 @@ import java.security.*;
 * @version $Id$
 *
 * @author Alan Mislove
+* @author Atul Singh
 */
 
-public class SplitStreamTest extends Test implements ISplitStreamApp, Observer {
-
-  private Credentials credentials = new PermissiveCredentials();
-  
-  private Scribe scribe;
-
-  private SplitStreamImpl splitStream;
-
-  private NodeId CHANNEL_NODE_ID;
-
-  private ChannelId CHANNEL_ID;
-
-  private int base = 16;
-
-  private String m_name = "SplitStreamTest";
-
-  private Hashtable m_channels = new Hashtable();
-
-  private int m_testFreq = Scribe.m_scribeMaintFreq;
-  
-  private int OUT_BW = 16;
-
-  private int last_recv_time = 0;
-
-  public Hashtable m_logTable = new Hashtable();
-
-  /**
-   * Hashtable storing sequence numbers being published per stripe
-   */
-  public Hashtable m_stripe_seq = new Hashtable();
-
-  /**
-    * Constructor which takes the local node this test is on,
-    * an array of all the nodes in the network, and a printwriter
-    * to which to write data.
-    *
-    * @param out The PrintWriter to write test results to.
-    * @param localNode The local Pastry node
-    * @param nodes NodeHandles to all of the other participating
-    *              TestHarness nodes (this test class ignores these)
-    */
-  public SplitStreamTest(PrintStream out, PastryNode localNode, TestHarness harness) {
-    super(out, localNode, harness);
-
-    ChannelId channelId;
-
-    scribe = new Scribe(localNode, credentials);
-    scribe.setTreeRepairThreshold(3);
-
-    CHANNEL_NODE_ID = scribe.generateTopicId(m_name);
-    CHANNEL_ID = new ChannelId(CHANNEL_NODE_ID);
-    splitStream = new SplitStreamImpl(localNode, scribe);
-    splitStream.registerApp(this);
+public class SplitStreamTest extends Test{
     
-    //_out.println("Created scribe/splitstram at " + localNode.getNodeId());
-  }	
-
-  /**
-    * Method which is called when the TestHarness wants this
-    * Test to begin testing.
-    */
-  public void startTest(NodeHandle[] nodes) {
-    if(getNodeId().equals(new NodeId(new byte[NodeId.nodeIdBitLength]))) {
-      // creator of channel
-	//_out.println("Creating channel at " + getNodeId() + " with name " + m_name);
-      createChannel(base, m_name);
-    }
-    else{
-	//_out.println("Attaching channel at " + getNodeId());
-      attachChannel(CHANNEL_ID);
-    }
-  }
- 
-  public Address getAddress() {
-    return SplitStreamTestAddress.instance();
-  }
-
-  public Credentials getCredentials() {
-    return credentials;
-  }
-
-  public void messageForAppl(Message msg) {
-    SplitStreamTestMessage tmsg = (SplitStreamTestMessage)msg;
-    tmsg.handleDeliverMessage( this);
-  }
-
-  /**
-    * Observer Implementation
-   */
-  public void update(Observable o, Object arg){
-    byte[] data = (byte [])arg;
-    //Byte bt = new Byte(data[0]);
-    String ds = new String(data);
-    StripeId stripeId = (StripeId)((Stripe)o).getStripeId();
-    String str = stripeId.toString().substring(3,4);
-    int recv_time = (int)System.currentTimeMillis();
-    int diff;
-    char [] c = str.toString().toCharArray();
-    int stripe_int = c[0] - '0';
-    if(stripe_int > 9)
-	stripe_int = 10 + c[0] - 'A';
-    else
-	stripe_int = c[0] - '0';
+    private Credentials credentials = new PermissiveCredentials();
     
-    if(last_recv_time == 0)
-      diff = 0;
-    else
-      diff = recv_time - last_recv_time;
+    private Scribe scribe;
     
-    last_recv_time = recv_time;
+    private SplitStreamImpl splitstream;
 
-    //_out.println("Application "+m_appIndex+" received data "+m_numRecv);
+    private SplitStreamTestClient ssclient;
+    
+    private NodeId CHANNEL_NODE_ID;
+    
+    private ChannelId CHANNEL_ID;
+    
+    private int base = 16;
+    
+    // every 1000 ms, publish data on each stripe
+    private int testFreq = 1000;
+    
+    private int OUT_BW = 16;
+    
+    private int last_recv_time = 0;
+    
+    public int sequenceNum = 0;
+    
+    public static String INSTANCE = "DistSplitStreamTest";
+    
+    // 320KB/sec total => 16 KB/sec/stripe
+    public int DATA_SIZE = 320;
+
+    public byte[] content = new byte[DATA_SIZE * 1024 / base];
     /**
-      * Logging style
-     * <App_Index> <Host-name> <Stripe_num> <Seq_num> <Diff>
-     */
-    _out.println(stripe_int+"\t"+Integer.valueOf(ds).intValue()+"\t"+recv_time);
-
-  }
-  
-  
-  /**
-    * This is a call back into the application
-   * to notify it that one of the stripes was unable to
-   * to find a parent, and thus unable to recieve data.
-   */
-  public void handleParentFailure(Stripe s){
-  }  
-
-  public void splitstreamIsReady(){
-  }
-
-  /**
-    * Upcall that given channel is ready, i.e. it has all the
-   * meta information for the channel, e.g. the number of
-   * stripes, no. of spare capacity trees and their ids etc.
-   */
-  public void channelIsReady(ChannelId channelId) {
-    Channel channel = (Channel)m_channels.get(CHANNEL_ID);
-    //_out.println("Channel " + CHANNEL_ID + " is ready, at " + getNodeId());
-    // join all the stripes for the channel
-    joinChannelStripes(CHANNEL_ID);
-
-
-    // Trigger the periodic invokation of DistScribeRegrTest message
-    _localNode.scheduleMsgAtFixedRate(makeSplitStreamTestMessage(credentials, CHANNEL_ID), 0 , m_testFreq*1000);
-  }
-
-  /**
-    * Create a channel with given channelId
-   *
-   */
-  public ChannelId createChannel(int numStripes, String name){
-    Channel channel = splitStream.createChannel(base, name);
-    m_channels.put(channel.getChannelId(), channel);
-    channel.registerApp(this);
-    channel.configureChannel(OUT_BW);
-    if(channel.isReady())
-      channelIsReady(channel.getChannelId());
-    return channel.getChannelId();
-  }
-
-  public void attachChannel(ChannelId channelId){
-    Channel channel = splitStream.attachChannel(channelId);
-    m_channels.put(channel.getChannelId(), channel);
-    channel.registerApp(this);
-    channel.configureChannel(OUT_BW);
-    if(channel.isReady())
-      channelIsReady(channel.getChannelId());
-    return;
-  }
-
-  public void showBandwidth(ChannelId channelId){
-    Channel channel = (Channel) m_channels.get(channelId);
-    BandwidthManager bandwidthManager = channel.getBandwidthManager();
-    //_out.println("Channel " + channelId + " has " +
-    //bandwidthManager.getUsedBandwidth(channel) + " children ");
-    return;
-  }
-
-  /**
-    * Sends data on all the stripes of channel represented by channelId.
-   */
-  public void sendData(ChannelId channelId){
-    Channel send = (Channel) m_channels.get(channelId);
-
-    for(int i = 0; i < send.getNumStripes(); i++){
-      StripeId stripeId = send.getStripes()[i];
-      Stripe stripe = send.joinStripe(stripeId, this);
-      OutputStream out = stripe.getOutputStream();
-      //_out.println("Sending on Stripe " + stripeId);
-      //	    byte[] toSend = "Hello".getBytes() ;
-
-      Integer seq = (Integer)m_stripe_seq.get((StripeId)stripeId);
-      if(seq == null)
-        seq = new Integer(0);
-      String str = seq.toString();
-      byte[] toSend = str.getBytes();
-      //toSend[0] = seq.byteValue();
-      int seq_num = seq.intValue();
-      seq = new Integer(seq_num + 1);
-      m_stripe_seq.put(stripeId, seq);
-      try{
-        out.write(toSend, 0, toSend.length );
-      }
-      catch(IOException e){
-        e.printStackTrace();
-      }
-    }
-  }
-
-  /**
-    * Join all stripes associated with the given channeld
-   */
-  public void joinChannelStripes(ChannelId channelId){
-    Channel channel =  (Channel) m_channels.get(channelId);
-
-    // Join all the stripes associated with the channel
-    while(channel.getNumSubscribedStripes() < channel.getNumStripes()){
-      Stripe stripe = channel.joinAdditionalStripe(this);
-    }
-  }
-
-  public boolean channelReady(ChannelId channelId){
-    Channel channel =  (Channel) m_channels.get(channelId);
-    return channel.isReady();
-  }
-
-  public boolean isRoot(NodeId topicId){
-    return scribe.isRoot(topicId);
-  }
-
-  public Scribe getScribe(){
-    return scribe;
-  }
-
-
-  public StripeId[] getStripeIds(ChannelId channelId){
-    Channel channel = (Channel) m_channels.get(channelId);
-    return channel.getStripes();
-  }
-
-  public int getNumStripes(ChannelId channelId){
-    Channel channel = (Channel) m_channels.get(channelId);
-    return channel.getNumStripes();
-  }
-
-  /**
-    * Makes a DistSplitStreamTest message.
-   *
-   * @param c the credentials that will be associated with the message
-   * @return the DistSplitStreamTestMessage
-   */
-  private Message makeSplitStreamTestMessage(Credentials c, ChannelId channelId) {
-    return new SplitStreamTestMessage(getAddress(), c, channelId );
-  }
-  
-  public static class SplitStreamTestAddress implements Address {
-
-    /**
-    * The only instance of SplitStreamTestAddress ever created.
-     */
-    private static SplitStreamTestAddress _instance;
-
-    /**
-    * Returns the single instance of TestHarnessAddress.
-     */
-    public static SplitStreamTestAddress instance() {
-      if(null == _instance) {
-        _instance = new SplitStreamTestAddress();
-      }
-      return _instance;
-    }
-
-    /**
-      * Code representing address.
-     */
-    public int _code = 0x3474922a;
-
-    /**
-      * Private constructor for singleton pattern.
-     */
-    private SplitStreamTestAddress() {}
-
-    /**
-      * Returns the code representing the address.
-     */
-    public int hashCode() { return _code; }
-
-    /**
-      * Determines if another object is equal to this one.
-     * Simply checks if it is an instance of AP3Address
-     * since there is only one instance ever created.
-     */
-    public boolean equals(Object obj) {
-      return (obj instanceof SplitStreamTestAddress);
-    }
-  }		
-
-  public static class SplitStreamTestMessage extends Message implements Serializable
-  {
-
-    private ChannelId m_channelId = null;
-    /**
-    * Constructor
+     * Constructor which takes the local node this test is on,
+     * an array of all the nodes in the network, and a printwriter
+     * to which to write data.
      *
-     * @param addr the address of the distSplitStreamTestApp receiver.
-     * @param c the credentials associated with the mesasge.
+     * @param out The PrintWriter to write test results to.
+     * @param localNode The local Pastry node
+     * @param nodes NodeHandles to all of the other participating
+     *              TestHarness nodes (this test class ignores these)
      */
-    public SplitStreamTestMessage( Address addr, Credentials c, ChannelId channelId) {
-        super( addr, c );
-        m_channelId = channelId;
-      }
+    public SplitStreamTest(PrintStream out, PastryNode localNode, TestHarness harness) {
+	super(out, localNode, harness);
+	splitstream = new SplitStreamImpl(localNode, INSTANCE);
+	ssclient = new SplitStreamTestClient(localNode, splitstream);
+	CHANNEL_ID = new ChannelId(generateId());
+	ssclient.attachChannel(CHANNEL_ID);
+	System.setOut(out);
+	//_out.println("Created scribe/splitstram at " + localNode.getNodeId()+", created channel "+CHANNEL_ID);
+    }	
 
     /**
-      * This method is called whenever the pastry node receives a message for the
-     * DistSplitStreamTestApp.
-     *
-     * @param splitStreamApp the DistSplitStreamTestApp application.
+     * Method which is called when the TestHarness wants this
+     * Test to begin testing.
      */
-    public void handleDeliverMessage(SplitStreamTest test) {
-      Channel channel = (Channel) test.m_channels.get(m_channelId);
-
-      if (test.getNodeId().equals(new NodeId(new byte[NodeId.nodeIdBitLength]))){                                         // I am the creator of the channel
-        test.sendData(channel.getChannelId());
-      }
+    public void startTest(NodeHandle[] nodes) {
+	ssclient.subscribeStripes();
+      // Trigger the periodic invokation of DistScribeRegrTest message
+	if (ssclient.getId().equals((Id)(new NodeId(new byte[NodeId.nodeIdBitLength])))){
+	    createDataToPublish();
+	    // start publishing data after 60 seconds to allow nodes to join the trees
+	    _localNode.scheduleMsgAtFixedRate(makeSplitStreamTestMessage(credentials), 60*1000 , testFreq);
+	    _out.println("DEBUG :: Scheduling message");
+	}
     }
 
-
-    public String toString() {
-      return new String( "SPLIT_STREAM_TEST  MSG:" );
+    /**
+     * Private method which generates a random Id
+     *
+     * @return A new random Id
+     */
+    private Id generateId() {
+	byte[] data = new byte[20];
+	//new Random(PastrySeed.getSeed()).nextBytes(data);
+	new Random(100).nextBytes(data);
+	return new Id(data);
     }
-  }
+
+    public void createDataToPublish(){
+	// if DATA_SIZE = 256 KB, it means, on each stripe we push
+	// 4KB
+	for(int j = 0 ; j < DATA_SIZE * 1024/base; j++){
+	    content[j] = (new Byte("0")).byteValue();
+	}
+    }
     
+    public Address getAddress() {
+	return SplitStreamTestAddress.instance();
+    }
+    
+    public Credentials getCredentials() {
+	return credentials;
+    }
+    
+    public void messageForAppl(Message msg) {
+	SplitStreamTestMessage tmsg = (SplitStreamTestMessage)msg;
+	tmsg.handleDeliverMessage(this);
+	_out.println("DEBUG :: Received message "+tmsg);
+    }
+    
+    public Channel getChannel(){
+	return this.ssclient.getChannel();
+    }
+    
+    public Id getId(){
+	return ssclient.getId();
+    }
+  /**
+   * Sends data on all the stripes of channel represented by channelId.
+   */
+    public void sendData(){
+	String str = (new Integer(sequenceNum)).toString();
+	str += "\t" + (new Integer((int)System.currentTimeMillis()).toString());
+	str += "\t";
+	byte[] toSend = new byte[DATA_SIZE * 1024 / base];
+	System.arraycopy(str.getBytes(), 0, toSend, 0, str.getBytes().length);
+	System.arraycopy(content, 0, toSend, str.getBytes().length + 1, toSend.length - (str.getBytes().length + 1));
+	sequenceNum++;
+	ssclient.publishAll(toSend);
+    }
+    
+    
+    private Message makeSplitStreamTestMessage(Credentials c) {
+	return new SplitStreamTestMessage(getAddress(), c);
+    }
+    
+    public static class SplitStreamTestAddress implements Address {
+	
+	/**
+	 * The only instance of SplitStreamTestAddress ever created.
+	 */
+	private static SplitStreamTestAddress _instance;
+	
+	/**
+	 * Returns the single instance of TestHarnessAddress.
+	 */
+	public static SplitStreamTestAddress instance() {
+	    if(null == _instance) {
+		_instance = new SplitStreamTestAddress();
+	    }
+	    return _instance;
+	}
+	
+	/**
+	 * Code representing address.
+	 */
+	public int _code = 0x3474922a;
+	
+	/**
+	 * Private constructor for singleton pattern.
+	 */
+	private SplitStreamTestAddress() {}
+	
+	/**
+	 * Returns the code representing the address.
+	 */
+	public int hashCode() { return _code; }
+	
+	/**
+	 * Determines if another object is equal to this one.
+	 * Simply checks if it is an instance of AP3Address
+	 * since there is only one instance ever created.
+	 */
+	public boolean equals(Object obj) {
+	    return (obj instanceof SplitStreamTestAddress);
+	}
+    }		
+    
+    public static class SplitStreamTestMessage extends Message implements Serializable
+    {
+	
+	/**
+	 * Constructor
+	 *
+	 * @param addr the address of the distSplitStreamTestApp receiver.
+	 * @param c the credentials associated with the mesasge.
+	 */
+	public SplitStreamTestMessage( Address addr, Credentials c) {
+	    super( addr, c );
+	}
+	
+	/**
+	 * This method is called whenever the pastry node receives a message for the
+	 * DistSplitStreamTestApp.
+	 *
+	 * @param splitStreamApp the DistSplitStreamTestApp application.
+	 */
+	public void handleDeliverMessage(SplitStreamTest test) {
+	    if (test.getId().equals(new NodeId(new byte[NodeId.nodeIdBitLength]))){
+		// I am the creator of the channel
+		test.sendData();
+	    }
+	}
+	
+	
+	public String toString() {
+	    return new String( "SPLIT_STREAM_TEST  MSG:" );
+	}
+    }
+    
+    private class SplitStreamTestClient implements SplitStreamClient{
+	
+	/** 
+	 * The underlying common api node
+	 *
+	 */
+	private Node n = null;
+	
+	/** 
+	 * The stripes for a channel 
+	 *
+	 */
+	private Stripe[] stripes;
+	
+	/** 
+	 * The channel to be used for this test
+	 *
+	 */
+	private Channel channel;
+	
+	/** 
+	 * The SplitStream service for this node 
+	 *
+	 */
+	private SplitStream ss;
+	
+	
+	private int numMesgsReceived = 0;
+	
+	private SplitStreamScribePolicy policy = null;
+	public SplitStreamTestClient(Node n, SplitStream ss){
+	    this.n = n;
+	    this.ss =ss;
+	    log("Client Created " + n);
+	}
+	
+	public Channel getChannel(){
+	    return this.channel;
+	}
+	public void joinFailed(Stripe s){
+	    log("Join Failed on " + s);
+	}
+	
+	public void deliver(Stripe s, byte[] data){
+	    //log("Data recieved on " + s);
+	    numMesgsReceived++;
+
+
+	    //Byte bt = new Byte(data[0]);
+	    String ds = new String(data);
+	    StringTokenizer tk = new StringTokenizer(ds);
+	    String seqNumber = tk.nextToken();
+	    String sentTime = tk.nextToken();
+	    Id stripeId = (rice.pastry.Id)(s.getStripeId().getId());
+	    String str = stripeId.toString().substring(3, 4);
+	    int recv_time = (int)System.currentTimeMillis();
+	    int diff;
+	    char [] c = str.toString().toCharArray();
+	    int stripe_int = c[0] - '0';
+	    if(stripe_int > 9)
+		stripe_int = 10 + c[0] - 'A';
+	    else
+		stripe_int = c[0] - '0';
+	    
+	    if(last_recv_time == 0)
+		diff = 0;
+	    else
+		diff = recv_time - last_recv_time;
+	    
+	    last_recv_time = recv_time;
+	    
+	    //_out.println("Application "+m_appIndex+" received data "+m_numRecv);
+	    /**
+	     * Logging style
+	     * <App_Index> <Host-name> <Stripe_num> <Seq_num> <Diff>
+	     */
+	    _out.println(stripe_int+"\t"+seqNumber+"\t"+"\t"+sentTime+"\t"+recv_time);
+	}
+	
+	
+
+	public void createChannel(ChannelId cid){
+	    log("Channel " + cid + " created."); 
+	    channel = ss.createChannel(cid);
+	    getStripes();
+	}
+	
+	public void attachChannel(ChannelId cid){
+	    log("Attaching to Channel " + cid + "."); 
+	    if(channel == null)
+		channel = ss.attachChannel(cid);
+	    getStripes();
+	}
+
+	public Stripe[] getStripes(){
+	    log("Retrieving Stripes.");
+	    stripes = channel.getStripes();
+	    return stripes;
+	}
+	
+	public void subscribeStripes(){
+	    log("Subscribing to all Stripes.");
+	    for(int i = 0; i < stripes.length ; i ++){
+		stripes[i].subscribe(this);
+	    } 
+	}
+	public void publishAll(byte[] b){
+	    log("Publishing to all Stripes.");
+	    for(int i = 0; i < stripes.length; i++){
+		publish(b, stripes[i]);
+	    }
+	}
+	public void publish(byte[] b, Stripe s){
+	    //log("Publishing to " + s);
+	    s.publish(b);
+	}
+	
+	public int getNumMesgs(){
+	    return numMesgsReceived;
+	}
+	
+	public void reset(){
+	  numMesgsReceived = 0;
+	}
+	
+	public Id getId(){
+	    return (rice.pastry.Id)channel.getLocalId();
+	}
+	private void log(String s){
+	    _out.println("DEBUG :: " + n + " " + s);
+	}
+	
+    }
+
 }
+
