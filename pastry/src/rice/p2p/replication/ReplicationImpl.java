7f8611703829e5823b5066ecab0f9e60a7986ec5
amislove <amislove@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2003-11-02 06:30:27 +0000

    first version of p2p replication
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1444 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/p2p/replication/ReplicationImpl.java
@@ -0,0 +1,272 @@
+/*************************************************************************
+
+"Free Pastry" Peer-to-Peer Application Development Substrate 
+
+Copyright 2002, Rice University. All rights reserved. 
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+- Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+- Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+- Neither  the name  of Rice  University (RICE) nor  the names  of its
+contributors may be  used to endorse or promote  products derived from
+this software without specific prior written permission.
+
+This software is provided by RICE and the contributors on an "as is"
+basis, without any representations or warranties of any kind, express
+or implied including, but not limited to, representations or
+warranties of non-infringement, merchantability or fitness for a
+particular purpose. In no event shall RICE or contributors be liable
+for any direct, indirect, incidental, special, exemplary, or
+consequential damages (including, but not limited to, procurement of
+substitute goods or services; loss of use, data, or profits; or
+business interruption) however caused and on any theory of liability,
+whether in contract, strict liability, or tort (including negligence
+or otherwise) arising in any way out of the use of this software, even
+if advised of the possibility of such damage.
+
+********************************************************************************/
+
+package rice.p2p.replication;
+
+import java.util.*;
+import java.util.logging.*;
+
+import rice.p2p.commonapi.*;
+import rice.p2p.replication.messaging.*;
+
+/**
+ * @(#) ReplicationImpl.java
+ *
+ * This is the implementation of the replication manager.
+ *
+ * @version $Id$
+ *
+ * @author Alan Mislove
+ */
+public class ReplicationImpl implements Replication, Application {
+  
+  /**
+   * The amount of time to wait between replications
+   */
+  public static int MAINTENANCE_INTERVAL = 180000;
+  
+  /**
+   * this application's endpoint
+   */
+  protected Endpoint endpoint;
+  
+  /**
+   * the logger which we will use
+   */
+  protected Logger log = Logger.getLogger(this.getClass().getName());
+  
+  /**
+   * the local node handle
+   */
+  protected NodeHandle handle;
+  
+  /**
+   * The factory for create IdSets and IdRanges
+   */
+  protected IdFactory factory;
+  
+  /**
+   * This replication's client
+   */
+  protected ReplicationClient client;
+  
+  /**
+   * The replication factor for this replication
+   */
+  protected int replicationFactor;
+  
+  /**
+   * Constructor
+   *
+   * @param node The node below this Replication implementation
+   * @param client The client for this Replication
+   * @param replicationFactor The replication factor for this instance
+   * @param instance The unique instance name of this Replication
+   */
+  public ReplicationImpl(Node node, ReplicationClient client, int replicationFactor, String instance) {
+    this.client = client;
+    this.replicationFactor = replicationFactor;
+    this.factory = node.getIdFactory();
+    
+    this.endpoint = node.registerApplication(this, instance);
+    this.handle = endpoint.getLocalNodeHandle();
+    
+    log.finer(endpoint.getId() + ": Starting up ReplicationImpl with client " + client + " and factor " + replicationFactor);
+    
+    // inject the first reminder message, which will cause the replication to begin
+    // and the next maintenance message to be scheduled
+    deliver(handle.getId(), new ReminderMessage(handle));
+  }
+  
+  /**
+   * Internal method which takes returns set A + set B, or all of the members
+   * of set A and set B.
+   *
+   * @param a The first set
+   * @param b The second set
+   * @return The merge, a+b
+   */
+  private IdSet merge(IdSet a, IdSet b) {
+    IdSet result = factory.buildIdSet();
+    Iterator i = a.getIterator();
+    
+    while (i.hasNext()) {
+      result.addId((Id) i.next());
+    }
+    
+    i = b.getIterator();
+    
+    while (i.hasNext()) {
+      result.addId((Id) i.next());
+    }
+    
+    return result;
+  }
+  
+  /**
+   * Internal method which takes returns set A - set B, or all of the members
+   * of set A which are not in set B.
+   *
+   * @param a The first set
+   * @param b The second set
+   * @return The difference, a-b
+   */
+  private IdSet difference(IdSet a, IdSet b) {
+    IdSet result = factory.buildIdSet();
+    Iterator i = a.getIterator();
+    
+    while (i.hasNext()) {
+      Id id = (Id) i.next();
+      
+      if (! b.isMemberId(id)) 
+        result.addId(id);
+    }
+    
+    return result;
+  }
+  
+  /**
+   * Returns the range for which the local node is an i root, where i can range between
+   * 0 and replicationFactor
+   *
+   * @return The *total* range
+   */
+  protected IdRange getTotalRange() {
+    return endpoint.range(handle, replicationFactor, handle.getId(), true);
+  }
+    
+  /**
+   * Internal method which updates the client about what his current range is
+   */
+  private void updateClient() {
+    log.fine(endpoint.getId() + ": Updating client with range " + getTotalRange());
+    client.setRange(getTotalRange());
+  }
+  
+  /**
+   * This internal method sends out the request messages to all of the nodes
+   * which hold keys this node may be interested in
+   */
+  public void sendRequests() {
+    log.finer(endpoint.getId() + ": Sending out requests"); 
+    NodeHandleSet handles = endpoint.neighborSet(Integer.MAX_VALUE);
+    
+    for (int i=0; i<handles.size(); i++) {
+      NodeHandle handle = handles.getHandle(i);
+      IdRange range = endpoint.range(handle, 0, handle.getId());
+
+      if ((range != null) && (! range.intersectRange(getTotalRange()).isEmpty())) {
+        log.finer(endpoint.getId() + ": Sending request to " + handle + " for range " + range);
+        RequestMessage request = new RequestMessage(this.handle, new IdRange[] {range.intersectRange(getTotalRange())});
+        endpoint.route(handle.getId(), request, handle);
+      }
+    }
+  }
+
+  
+  // ----- COMMON API METHODS -----
+  
+  /**
+   * This method is invoked on applications when the underlying node
+   * is about to forward the given message with the provided target to
+   * the specified next hop.  Applications can change the contents of
+   * the message, specify a different nextHop (through re-routing), or
+   * completely terminate the message.
+   *
+   * @param message The message being sent, containing an internal message
+   * along with a destination key and nodeHandle next hop.
+   *
+   * @return Whether or not to forward the message further
+   */
+  public boolean forward(RouteMessage message) {
+    return true;
+  }
+  
+  /**
+   * This method is called on the application at the destination node
+   * for the given id.
+   *
+   * @param id The destination id of the message
+   * @param message The message being sent
+   */
+  public void deliver(Id id, Message message) {
+    log.fine(endpoint.getId() + ": Received message " + message);
+    
+    if (message instanceof RequestMessage) {
+      RequestMessage rm = (RequestMessage) message;
+      IdSet response = factory.buildIdSet();
+      
+      for (int i=0; i<rm.getRanges().length; i++)
+        response = merge(response, client.scan(rm.getRanges()[i]));
+      
+      endpoint.route(rm.getSource().getId(), new ResponseMessage(handle, response), rm.getSource());
+    } else if (message instanceof ResponseMessage) {
+      ResponseMessage rm = (ResponseMessage) message;
+      IdSet keys = rm.getIdSet();
+      IdSet exist = client.scan(getTotalRange());
+      IdSet fetch = difference(keys, exist);
+        
+      if (fetch.numElements() > 0) 
+        client.fetch(fetch);
+    } else if (message instanceof ReminderMessage) {
+      sendRequests();
+      endpoint.scheduleMessage(message, MAINTENANCE_INTERVAL);
+      updateClient(); 
+    } else {
+      log.warning(endpoint.getId() + ": Received unknown message " + message + " - dropping on floor.");
+    }
+  }
+  
+  /**
+   * This method is invoked to inform the application that the given node
+   * has either joined or left the neighbor set of the local node, as the set
+   * would be returned by the neighborSet call.
+   *
+   * @param handle The handle that has joined/left
+   * @param joined Whether the node has joined or left
+   */
+  public void update(NodeHandle handle, boolean joined) {
+  }
+  
+}
+
+
+
+
+
+
+
+
---
+++ pastry/src/rice/p2p/replication/testing/ReplicationRegrTest.java
@@ -0,0 +1,297 @@
+package rice.p2p.replication.testing;
+
+import java.io.Serializable;
+import java.net.*;
+
+import java.util.*;
+
+import rice.*;
+import rice.Continuation.*;
+
+import rice.persistence.*;
+
+import rice.p2p.commonapi.*;
+import rice.p2p.commonapi.testing.*;
+import rice.p2p.replication.*;
+
+/**
+ * @(#) ReplicationRegrTest.java Provides regression testing for the replication service using distributed
+ * nodes.
+ *
+ * @version $Id$
+ * @author Alan Mislove
+ */
+public class ReplicationRegrTest extends CommonAPITest {
+
+  /**
+   * The replication factor to use
+   */
+  public static int REPLICATION_FACTOR = 3;
+  
+  /**
+   * the instance name to use
+   */
+  public static String INSTANCE = "ReplicationRegrTest";
+
+  /**
+   * the replication impls in the ring
+   */
+  protected ReplicationImpl[] replications;
+
+  /**
+   * The clients
+   */
+  protected TestReplicationClient[] clients;
+
+  /**
+   * random number generator
+   */
+  protected Random rng;
+
+  /**
+   * Constructor which sets up all local variables
+   */
+  public ReplicationRegrTest() {
+    replications = new ReplicationImpl[NUM_NODES];
+    clients = new TestReplicationClient[NUM_NODES];
+    rng = new Random();
+  }
+
+
+  /**
+   * Usage: ReplicationRegrTest [-port p] [-bootstrap host[:port]] [-nodes n] [-protocol (rmi|wire)]
+   * [-help]
+   *
+   * @param args DESCRIBE THE PARAMETER
+   */
+  public static void main(String args[]) {
+    parseArgs(args);
+    ReplicationRegrTest test = new ReplicationRegrTest();
+    test.start();
+  }
+
+  /**
+   * Method which should process the given newly-created node
+   *
+   * @param node The newly created node
+   * @param num The number of this node
+   */
+  protected void processNode(int num, Node node) {
+    clients[num] = new TestReplicationClient(node);
+    replications[num] = new ReplicationImpl(node, clients[num], REPLICATION_FACTOR, INSTANCE);
+  }
+
+  /**
+   * Method which should run the test - this is called once all of the nodes have been created and
+   * are ready.
+   */
+  protected void runTest() {
+    testBasic();
+    testMaintenance();
+  }
+
+  /*
+   *  ---------- Test methods and classes ----------
+   */
+  
+  /**
+    * Tests basic functionality
+   */
+  public void testBasic() {
+    int num = rng.nextInt(NUM_NODES);
+    Id id = nodes[num].getId();
+    
+    IdRange all = FACTORY.buildIdRange(FACTORY.buildId(new byte[20]), FACTORY.buildId(new byte[20]));
+    
+    sectionStart("Testing Basic Functionality");
+    
+    stepStart("Inserting Object");
+    
+    clients[num].insert(id);
+    
+    stepDone(SUCCESS);
+    
+    stepStart("Initiating Maintenance");
+    
+    runMaintenance();
+    simulate();
+    
+    int count = 0;
+    
+    for (int i=0; i<NUM_NODES; i++)  {
+      if (clients[i].scan(all).isMemberId(id)) 
+        count++;
+    }
+  
+    assertTrue("Correct number of replicas should be " + (REPLICATION_FACTOR + 1) + " was " + count, 
+               count == REPLICATION_FACTOR + 1);
+    
+    stepDone(SUCCESS);
+    
+    sectionDone();
+  }
+  
+  /**
+    * Tests maintenance functionality
+   */
+  public void testMaintenance() {
+    int num = rng.nextInt(NUM_NODES);
+    Id id = nodes[num].getId();
+    
+    IdRange all = FACTORY.buildIdRange(FACTORY.buildId(new byte[20]), FACTORY.buildId(new byte[20]));
+    
+    sectionStart("Testing Basic Functionality");
+    
+    stepStart("Inserting Object");
+    
+    clients[num].insert(id);
+    
+    stepDone(SUCCESS);
+    
+    stepStart("Initiating Maintenance");
+    
+    runMaintenance();
+    simulate();
+    
+    int count = 0;
+    
+    for (int i=0; i<NUM_NODES; i++)  {
+      if (clients[i].scan(all).isMemberId(id)) 
+        count++;
+    }
+    
+    assertTrue("Correct number of replicas should be " + (REPLICATION_FACTOR + 1) + " was " + count, 
+               count == REPLICATION_FACTOR + 1);
+    
+    stepDone(SUCCESS);
+    
+    stepStart("Killing Primary Replica");
+    
+    kill(num);
+    
+    stepDone(SUCCESS);
+    
+    stepStart("Initiating Maintenance");
+    
+    runMaintenance();
+    simulate();
+    
+    count = 0;
+    
+    for (int i=0; i<NUM_NODES; i++)  {
+      if (clients[i].scan(all).isMemberId(id)) 
+        count++;
+    }
+    
+    assertTrue("Correct number of replicas should be " + (REPLICATION_FACTOR + 2) + " was " + count, 
+               count == REPLICATION_FACTOR + 2);
+    
+    stepDone(SUCCESS);
+
+    sectionDone();
+  }
+  
+  public void runMaintenance() {
+    for (int i=0; i<NUM_NODES; i++) {
+      replications[i].sendRequests();
+    }
+    
+    simulate();
+  }
+
+  /**
+   * Private method which generates a random Id
+   *
+   * @return A new random Id
+   */
+  private Id generateId() {
+    byte[] data = new byte[20];
+    rng.nextBytes(data);
+    return FACTORY.buildId(data);
+  }
+
+  /**
+   * DESCRIBE THE CLASS
+   *
+   * @version $Id$
+   * @author amislove
+   */
+  protected class TestReplicationClient implements ReplicationClient {
+    
+    public MemoryStorage storage;
+    
+    public Node node;
+    
+    public TestReplicationClient(Node node) {
+      this.storage = new MemoryStorage(FACTORY);
+      this.node = node;
+    }
+    
+    /**
+     * This upcall is invoked to notify the application that is should
+     * fetch the cooresponding keys in this set, since the node is now
+     * responsible for these keys also.
+     *
+     * @param keySet set containing the keys that needs to be fetched
+     */
+    public void fetch(IdSet keySet) {
+      Iterator i = keySet.getIterator();
+      
+      while (i.hasNext()) {
+        Id next = (Id) i.next();
+        storage.store(next, next, new ListenerContinuation("Insertion of " + next));
+      }
+    }
+    
+    /**
+     * This upcall is to notify the application of the range of keys for 
+     * which it is responsible. The application might choose to react to 
+     * call by calling a scan(complement of this range) to the persistance
+     * manager and get the keys for which it is not responsible and
+     * call delete on the persistance manager for those objects.
+     *
+     * @param range the range of keys for which the local node is currently 
+     *              responsible  
+     */
+    public void setRange(IdRange range) {
+      IdRange notRange = range.getComplementRange();
+      IdSet set = storage.scan(notRange);
+      
+      Iterator i = set.getIterator();
+      
+      while (i.hasNext()) {
+        Id next = (Id) i.next();
+        storage.unstore(next, new ListenerContinuation("Removal of " + next));
+      }
+    }
+    
+    /**
+     * This upcall should return the set of keys that the application
+     * currently stores in this range. Should return a empty IdSet (not null),
+     * in the case that no keys belong to this range.
+     *
+     * @param range the requested range
+     */
+    public IdSet scan(IdRange range) {
+      return storage.scan(range);
+    }
+    
+    public void insert(Id id) {
+      storage.store(id, id, new ListenerContinuation("Insertion of id " + id));
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
