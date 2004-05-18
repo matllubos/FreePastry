f3ccd0e5aba8d55303d1fc006a90b8f5bf846b4e
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-05-18 17:49:57 +0000

    added files
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1556 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/pastry/socket/exception/AcceptFailedException.java
@@ -0,0 +1,19 @@
+/*
+ * Created on May 6, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package rice.pastry.socket.exception;
+
+import java.io.IOException;
+
+/**
+ * @author jeffh
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class AcceptFailedException extends IOException {
+
+}
---
+++ pastry/src/rice/pastry/socket/exception/SocketClosedByRemoteHostException.java
@@ -0,0 +1,21 @@
+/*
+ * Created on Apr 12, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package rice.pastry.socket.exception;
+
+import java.io.IOException;
+
+/**
+ * @author jeffh
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class SocketClosedByRemoteHostException extends IOException {
+  public SocketClosedByRemoteHostException(String s) {
+    super(s);
+  }
+}
