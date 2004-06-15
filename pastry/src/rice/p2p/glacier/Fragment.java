51cb158e25ec8df90177e3314a747c1874a7b7d0
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-06-15 19:50:27 +0000

    - Byte payload (instead of int) - Reformatted
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1667 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/p2p/glacier/Fragment.java
+++ pastry/src/rice/p2p/glacier/Fragment.java
@@ -2,14 +2,31 @@ package rice.p2p.glacier;
 
 import java.io.Serializable;
 
+/**
+ * DESCRIBE THE CLASS
+ *
+ * @version $Id$
+ * @author ahae
+ */
 public class Fragment implements Serializable {
-    public int fragmentID;
-    public int payload[];
-    
-    public Fragment(int _fragmentID, int _size)
-    {
-        fragmentID = _fragmentID;
-        payload = new int[_size];
-    }
-};
+  /**
+   * DESCRIBE THE FIELD
+   */
+  public int fragmentID;
+  /**
+   * DESCRIBE THE FIELD
+   */
+  public byte payload[];
+
+  /**
+   * Constructor for Fragment.
+   *
+   * @param _fragmentID DESCRIBE THE PARAMETER
+   * @param _size DESCRIBE THE PARAMETER
+   */
+  public Fragment(int _fragmentID, int _size) {
+    fragmentID = _fragmentID;
+    payload = new byte[_size];
+  }
+}
 
