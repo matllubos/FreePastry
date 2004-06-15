75a19012f0b9003d1e8ad94a654e63524f2a0605
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-06-15 19:51:08 +0000

    - New semantics (former StorageKey) - Reformatted
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1668 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/p2p/glacier/FragmentKey.java
+++ pastry/src/rice/p2p/glacier/FragmentKey.java
@@ -1,67 +1,196 @@
 package rice.p2p.glacier;
 
-import rice.p2p.commonapi.Id;
-import java.io.Serializable;
+import rice.p2p.commonapi.*;
+import rice.p2p.glacier.VersionKey;
 
-public class FragmentKey implements Serializable, Comparable {
-    protected Id owner;
-    protected Id id;
-    
-    public FragmentKey(Id owner, Id id)
-    {
-        this.id = id;
-        this.owner = owner;
-    }
-    
-    public Id getOwner()
-    {
-        return owner;
-    }
-    
-    public Id getId()
-    {
-        return id;
-    }
-    
-    public boolean equals(Object peer)
-    {
-        if (!(peer instanceof FragmentKey))
-            return false;
-            
-        FragmentKey fk = (FragmentKey)peer;
-        return (fk.owner.equals(this.owner) && fk.id.equals(this.id));
-    }
-    
-    public String toString()
-    {
-        return owner.toString()+"."+id.toString();
+/**
+ * DESCRIBE THE CLASS
+ *
+ * @version $Id$
+ * @author ahae
+ */
+public class FragmentKey implements Id, Comparable {
+  /**
+   * DESCRIBE THE FIELD
+   */
+  protected VersionKey key;
+  /**
+   * DESCRIBE THE FIELD
+   */
+  protected int id;
+
+  /**
+   * Constructor for FragmentKey.
+   *
+   * @param key DESCRIBE THE PARAMETER
+   * @param id DESCRIBE THE PARAMETER
+   */
+  public FragmentKey(VersionKey key, int id) {
+    this.id = id;
+    this.key = key;
+  }
+
+  /**
+   * Gets the Between attribute of the FragmentKey object
+   *
+   * @param ccw DESCRIBE THE PARAMETER
+   * @param cw DESCRIBE THE PARAMETER
+   * @return The Between value
+   */
+  public boolean isBetween(Id ccw, Id cw) {
+    System.err.println("FragmentKey::isBetween() called");
+    System.exit(1);
+    return false;
+  }
+
+  /**
+   * Gets the VersionKey attribute of the FragmentKey object
+   *
+   * @return The VersionKey value
+   */
+  public VersionKey getVersionKey() {
+    return key;
+  }
+
+  /**
+   * Gets the FragmentID attribute of the FragmentKey object
+   *
+   * @return The FragmentID value
+   */
+  public int getFragmentID() {
+    return id;
+  }
+
+  /**
+   * Gets the PeerKey attribute of the FragmentKey object
+   *
+   * @param otherId DESCRIBE THE PARAMETER
+   * @return The PeerKey value
+   */
+  public FragmentKey getPeerKey(int otherId) {
+    return new FragmentKey(key, otherId);
+  }
+
+  /**
+   * DESCRIBE THE METHOD
+   *
+   * @param peer DESCRIBE THE PARAMETER
+   * @return DESCRIBE THE RETURN VALUE
+   */
+  public boolean equals(Object peer) {
+    if (!(peer instanceof FragmentKey)) {
+      return false;
     }
 
-    public String toStringFull()
-    {
-        return owner.toStringFull()+"-"+id.toStringFull();
-    }
-    
-    public int compareTo(Object o)
-    {
-        if (((FragmentKey)o).owner == null) {
-            System.err.println("other owner = null");
-            System.exit(1);
-        }
+    FragmentKey sk = (FragmentKey) peer;
+    return (sk.key.equals(this.key) && (sk.id == this.id));
+  }
 
-        if (owner == null) {
-            System.err.println("my owner = null");
-            System.exit(1);
-        }
+  /**
+   * DESCRIBE THE METHOD
+   *
+   * @return DESCRIBE THE RETURN VALUE
+   */
+  public byte[] toByteArray() {
+    System.err.println("FragmentKey::toByteArray() called");
+    System.exit(1);
+    return null;
+  }
 
-        int ownerResult = owner.compareTo(((FragmentKey)o).owner);
-        if (ownerResult != 0)
-            return ownerResult;
-            
-        return id.compareTo(((FragmentKey)o).id);
+  /**
+   * DESCRIBE THE METHOD
+   *
+   * @return DESCRIBE THE RETURN VALUE
+   */
+  public String toStringFull() {
+    return key.toStringFull() + ":" + id;
+  }
+
+  /**
+   * DESCRIBE THE METHOD
+   *
+   * @return DESCRIBE THE RETURN VALUE
+   */
+  public String toString() {
+    return key.toString() + ":" + id;
+  }
+
+  /**
+   * DESCRIBE THE METHOD
+   *
+   * @param nid DESCRIBE THE PARAMETER
+   * @return DESCRIBE THE RETURN VALUE
+   */
+  public Distance longDistanceFromId(Id nid) {
+    System.err.println("FragmentKey::longDistanceFromId() called");
+    System.exit(1);
+    return null;
+  }
+
+  /**
+   * DESCRIBE THE METHOD
+   *
+   * @param nid DESCRIBE THE PARAMETER
+   * @return DESCRIBE THE RETURN VALUE
+   */
+  public Distance distanceFromId(Id nid) {
+    System.err.println("FragmentKey::distanceFromId() called");
+    System.exit(1);
+    return null;
+  }
+
+  /**
+   * Adds a feature to the ToId attribute of the FragmentKey object
+   *
+   * @param offset The feature to be added to the ToId attribute
+   * @return DESCRIBE THE RETURN VALUE
+   */
+  public Id addToId(Distance offset) {
+    System.err.println("FragmentKey::addToId() called");
+    System.exit(1);
+    return null;
+  }
+
+  /**
+   * DESCRIBE THE METHOD
+   *
+   * @param nid DESCRIBE THE PARAMETER
+   * @return DESCRIBE THE RETURN VALUE
+   */
+  public boolean clockwise(Id nid) {
+    System.err.println("FragmentKey::clockwise() called");
+    System.exit(1);
+    return false;
+  }
+
+  /**
+   * DESCRIBE THE METHOD
+   *
+   * @param o DESCRIBE THE PARAMETER
+   * @return DESCRIBE THE RETURN VALUE
+   */
+  public int compareTo(Object o) {
+    int keyResult = key.compareTo(((FragmentKey) o).key);
+    if (keyResult != 0) {
+      return keyResult;
     }
-    
-    public int hashCode() {
-        return (id.hashCode() + owner.hashCode());
+
+    if (this.id < ((FragmentKey) o).id) {
+      return -1;
     }
+    if (this.id > ((FragmentKey) o).id) {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  /**
+   * DESCRIBE THE METHOD
+   *
+   * @return DESCRIBE THE RETURN VALUE
+   */
+  public int hashCode() {
+    return (key.hashCode() + id);
+  }
 }
