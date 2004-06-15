bcd1585f12e5bc3b777ceaa6ae8969d51f0f7378
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-06-15 19:51:28 +0000

    - Interface changes (OSDI submission)
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1669 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/p2p/glacier/Glacier.java
+++ pastry/src/rice/p2p/glacier/Glacier.java
@@ -1,8 +1,15 @@
 package rice.p2p.glacier;
 
+import java.io.Serializable;
 import rice.*;
+import rice.p2p.past.PastContent;
+import rice.p2p.glacier.*;
 import rice.p2p.commonapi.*;
 
 public interface Glacier {
+
+  public Authenticator getAuthenticator(Serializable obj);
+  
+  public void insert(final PastContent obj, final StorageManifest manifest, final Continuation command) throws InvalidManifestException; 
 }
 
