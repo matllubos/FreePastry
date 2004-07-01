20e3d3304530d67291c953e462080f429bb02324
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-07-01 20:26:13 +0000

    added TooManyMessagesException
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1695 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/pastry/socket/exception/TooManyMessagesException.java
@@ -0,0 +1,36 @@
+/*
+ * Created on May 31, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package rice.pastry.socket.exception;
+
+/**
+ * @author jeffh
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class TooManyMessagesException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	public TooManyMessagesException() {
+		super();
+	}
+
+	public TooManyMessagesException(String arg0) {
+		super(arg0);
+	}
+
+	public TooManyMessagesException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public TooManyMessagesException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+}
