52cf7cbd27b14104806d626bf60fb5efc94f0cf9
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-07-22 04:05:16 +0000

    - Removed methods temporarily
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1758 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/p2p/glacier/Glacier.java
+++ pastry/src/rice/p2p/glacier/Glacier.java
@@ -7,9 +7,5 @@ import rice.p2p.glacier.*;
 import rice.p2p.commonapi.*;
 
 public interface Glacier {
-
-  public Authenticator getAuthenticator(Serializable obj);
-  
-  public void insert(final PastContent obj, final StorageManifest manifest, final Continuation command) throws InvalidManifestException; 
 }
 
