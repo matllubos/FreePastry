24e274f94031d6ed0ef230415c85e85a41892207
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-07-22 03:58:32 +0000

    - Moved to a subdirectory
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1753 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/p2p/glacier/InvalidManifestException.java
+++
@@ -1,8 +0,0 @@
-package rice.p2p.glacier;
-
-public class InvalidManifestException extends GlacierException {
-  public InvalidManifestException(String msg)
-  {
-    super(msg);
-  }
-}
--- pastry/src/rice/p2p/glacier/RestoreInfo.java
+++
@@ -1,7 +0,0 @@
-package rice.p2p.glacier;
-
-import rice.p2p.glacier.*;
-
-public class RestoreInfo {
-
-};
