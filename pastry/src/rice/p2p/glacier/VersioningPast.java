4337e2a0ad75c34f5b5ccbd6876190ce571c4d42
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-07-23 23:52:29 +0000

    - Added lookupHandles() and refresh()
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1803 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/p2p/glacier/VersioningPast.java
+++ pastry/src/rice/p2p/glacier/VersioningPast.java
@@ -27,5 +27,47 @@ public interface VersioningPast {
    */
   public void lookup(Id id, long version, Continuation command);
 
+  /**
+   * Retrieves the handles of up to max replicas of the object stored
+   * in this instance of Past with the given ID.  Asynchronously
+   * returns an array of PastContentHandles as the result to the
+   * provided Continuation, or a PastException.  
+   * 
+   * Each replica handle is obtained from a different primary storage
+   * root for the the given key. If max exceeds the replication factor
+   * r of this Past instance, only r replicas are returned.
+   *
+   * This method will return a PastContentHandle[] array containing all
+   * of the handles.
+   *
+   * @param id the key to be queried
+   * @param version the requested version
+   * @param max the maximal number of replicas requested
+   * @param command Command to be performed when the result is received 
+   */
+  public void lookupHandles(Id id, long version, int num, Continuation command);
+
+  /**
+   * Updates the objects stored under the provided keys id to expire no
+   * earlier than the provided expiration time.  Asyncroniously returns
+   * the result to the caller via the provided continuation.  
+   *
+   * The result of this operation is an Object[], which is the same length
+   * as the input array of Ids.  Each element in the array is either 
+   * Boolean(true), representing that the refresh succeeded for the 
+   * cooresponding Id, or an Exception describing why the refresh failed.  
+   * Specifically, the possible exceptions which can be returned are:
+   * 
+   * ObjectNotFoundException - if no object was found under the given key
+   * RefreshFailedException - if the refresh operation failed for any other
+   *   reason (the getMessage() will describe the failure)
+   * 
+   * @param id The keys which to refresh
+   * @param version The versions which to refresh
+   * @param expiration The time to extend the lifetime to
+   * @param command Command to be performed when the result is received
+   */
+  public void refresh(Id[] ids, long[] versions, long expiration, Continuation command);
+  
 }
 
