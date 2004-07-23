922783fce64ccff1561ee4d574b63c6c3c6c606d
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-07-23 23:51:24 +0000

    - Added
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1801 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/p2p/glacier/v2/GlacierContentHandle.java
@@ -0,0 +1,47 @@
+package rice.p2p.glacier.v2;
+
+import java.io.Serializable;
+
+import rice.*;
+import rice.p2p.past.PastContentHandle;
+import rice.p2p.past.gc.GCPastContentHandle;
+import rice.p2p.commonapi.*;
+
+public class GlacierContentHandle implements PastContentHandle, GCPastContentHandle {
+
+  protected Id id;
+  protected NodeHandle nodeHandle;
+  protected Manifest manifest;
+  protected long version;
+
+  public GlacierContentHandle(Id id, long version, NodeHandle nodeHandle, Manifest manifest) {
+    this.id = id;
+    this.nodeHandle = nodeHandle;
+    this.manifest = manifest;
+  }
+
+  public Id getId() {
+    return id;
+  }
+  
+  public NodeHandle getNodeHandle() {
+    return nodeHandle;
+  }
+  
+  public long getVersion() {
+    return version;
+  }
+  
+  public long getExpiration() {
+    return manifest.expirationDate;
+  }
+  
+  public Manifest getManifest() {
+    return manifest;
+  }
+}
+
+
+
+
+
