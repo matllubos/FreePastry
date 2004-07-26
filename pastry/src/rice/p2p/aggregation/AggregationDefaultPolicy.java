98e47f0ef9e1eef50ff0cba494ee1012cd3ef92c
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-07-26 22:37:39 +0000

    - Always aggregate by default
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1835 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/p2p/aggregation/AggregationDefaultPolicy.java
+++ pastry/src/rice/p2p/aggregation/AggregationDefaultPolicy.java
@@ -4,10 +4,8 @@ import rice.p2p.past.PastContent;
 
 public class AggregationDefaultPolicy implements AggregationPolicy {
 
-  private static final int aggregateComponentThreshold = 100*1024;
-
   public boolean shouldBeAggregated(PastContent obj, int size) {
-    return (!obj.isMutable() && (size<aggregateComponentThreshold));
+    return true;
   }
     
 }
