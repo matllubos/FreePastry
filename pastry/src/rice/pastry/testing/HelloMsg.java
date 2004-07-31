25d4730f07a74f84db2df8eea7e9401edb3ea0a3
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-07-31 00:31:26 +0000

    removed DistHelloWorldMT and moved it to pastry.socket.testing
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1880 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/pastry/testing/HelloMsg.java
+++ pastry/src/rice/pastry/testing/HelloMsg.java
@@ -14,26 +14,15 @@ import rice.pastry.messaging.Message;
  *
  */
 public class HelloMsg extends Message {
-  private transient boolean ackReceived = false;
-//	public transient int state;
-  public NodeHandle source;
-  public NodeHandle intermediateSource;
-    public Id target;
-    public NodeHandle actualReceiver;
-    private int msgid;
-//    private transient Vector receivers;
-//    private transient ConnectionManager lastMan;
-//    public transient SocketManager lastSM;
-    public transient NodeHandle nextHop = null;
-    public boolean messageDirect = false;
+  public Id target;
+  private int msgid;
+  public boolean messageDirect = false;
 
-    public HelloMsg(Address addr, NodeHandle src, Id tgt, int mid) {
-        super(addr);
-        source = src;
-        intermediateSource = src;
-        target = tgt;
-        msgid = mid;
-    }
+  public HelloMsg(Address addr, NodeHandle src, Id tgt, int mid) {
+    super(addr);
+    target = tgt;
+    msgid = mid;
+  }
 
   public String toString() {
     return "Hello #" + msgid;
@@ -47,52 +36,12 @@ public class HelloMsg extends Message {
     } else {
       s+=" routed";
     }
-    s += " lastAt:"+intermediateSource+" nextHop:"+nextHop+" from:" + source + " to:" + target;// +"<"+state+">"; // + " received by "+actualReceiver+"}";
-//    System.out.println("HM.getInfo():1");
-    /*
-    if (receivers != null) {
-      synchronized(receivers) {
-        Iterator i = receivers.iterator();
-        while (i.hasNext()) 
-          s += ","+i.next();      
-      }
-    }
-    */
-//    System.out.println("HM.getInfo():2");
-//    if (lastMan != null) {
-//      s += ","+lastMan.getStatus();
-//    }
-//    if (lastSM != null) {
-//      s += ", lastSM:"+lastSM;
-////      if (lastSM.closedTrace != null)
-////        lastSM.closedTrace.printStackTrace();
-//    }
-  //  System.out.println("HM.getInfo():3");
+//    s += " lastAt:"+intermediateSource+" nextHop:"+nextHop+" from:" + source + " to:" + target;// +"<"+state+">"; // + " received by "+actualReceiver+"}";
     return s;
-    }
+  }
     
-    public int getId(){
-        return msgid;
-    }
-
-		/**
-		 * @param address
-		 */
-//		public void addReceiver(InetSocketAddress address) {
-//      if (receivers == null) {
-//        receivers = new Vector();
-//      }
-//      receivers.add(address);
-//		}
-
-		/**
-		 * 
-		 */
-//		public InetSocketAddress getLastAddress() {
-//      if (receivers == null) {
-//        return null;
-//      }
-//			return (InetSocketAddress)receivers.lastElement();			
-//		}
+  public int getId(){
+      return msgid;
+  }
 
 }
