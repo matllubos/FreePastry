d3d31d8c2670ec9cec669decd75699b0e7276e35
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-08-03 02:34:24 +0000

    - New panels for GlacierV2 and Aggregation
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1904 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/visualization/server/AggregationPanelCreator.java
@@ -0,0 +1,115 @@
+package rice.visualization.server;
+
+import rice.visualization.data.*;
+import rice.pastry.*;
+import rice.p2p.aggregation.*;
+import rice.persistence.*;
+import rice.Continuation.*;
+
+import java.awt.*;
+import java.util.*;
+
+public class AggregationPanelCreator implements PanelCreator {
+  
+  public static final int UPDATE_TIME = 5000;
+  public static final int AGGR_OBJ_MULTIPLIER = 12;
+  public static final int WAITING_HISTORY = 240;
+  public static final int AGGR_OBJ_HISTORY = 240;
+  
+  int[] waitingHistory = new int[WAITING_HISTORY];
+  int[] aggregatesHistory = new int[AGGR_OBJ_HISTORY];
+  int[] objectsHistory = new int[AGGR_OBJ_HISTORY];
+  int waitingPtr = 0, aggrObjPtr = 0, seqNo = 0;
+  AggregationImpl aggregation;
+  
+  public AggregationPanelCreator(AggregationImpl aggregation) {
+    this.aggregation = aggregation;
+
+    Arrays.fill(aggregatesHistory, aggregation.getNumAggregates());
+    Arrays.fill(objectsHistory, aggregation.getNumObjectsInAggregates());
+
+    Thread t = new Thread("Aggregation Panel Monitor Thread") {
+      public void run() {
+        while (true) {
+          try {
+            updateData();
+            Thread.currentThread().sleep(UPDATE_TIME);
+          } catch (InterruptedException e) {
+          }
+        }
+      }
+    };
+      
+    t.start();
+  }
+  
+  public DataPanel createPanel(Object[] objects) {
+
+    DataPanel summaryPanel = new DataPanel("Aggregation");
+
+    GridBagConstraints cons = new GridBagConstraints();
+    cons.gridx = 0;
+    cons.gridy = 0;
+    cons.fill = GridBagConstraints.HORIZONTAL;
+    
+    KeyValueListView aggregationView = new KeyValueListView("Aggregation Overview", 380, 200, cons);
+    aggregationView.add("Obj. waiting", ""+aggregation.getNumObjectsWaiting());
+    aggregationView.add("Aggr. total", ""+aggregation.getNumAggregates());
+    aggregationView.add("Obj. in aggr", ""+aggregation.getNumObjectsInAggregates());
+    aggregationView.add("Obj. store", aggregation.getObjectStore().getClass().getName());
+    aggregationView.add("Aggr. store", aggregation.getAggregateStore().getClass().getName());
+    
+    rice.p2p.commonapi.Id currentHandle = (rice.p2p.commonapi.Id) aggregation.getHandle();
+    aggregationView.add("Current root", ((currentHandle == null) ? "null" : ((rice.p2p.multiring.RingId)currentHandle).getId().toStringFull()));
+
+    GridBagConstraints graphCons = new GridBagConstraints();
+    graphCons.gridx = 1;
+    graphCons.gridy = 0;
+    graphCons.fill = GridBagConstraints.HORIZONTAL;
+
+    LineGraphView graphView = new LineGraphView("Waiting list", 380, 200, graphCons, "Seconds", "Objects", false);
+    double[] waitingSeries = new double[WAITING_HISTORY];
+    double[] timeSeries = new double[WAITING_HISTORY];
+    for (int i=0; i<WAITING_HISTORY; i++) {
+      waitingSeries[i] = waitingHistory[(waitingPtr+i) % WAITING_HISTORY];
+      timeSeries[i] = (i*UPDATE_TIME)/1000;
+    }
+    graphView.addSeries("Waiting", timeSeries, waitingSeries, Color.blue);
+
+    GridBagConstraints graphCons2 = new GridBagConstraints();
+    graphCons2.gridx = 2;
+    graphCons2.gridy = 0;
+    graphCons2.fill = GridBagConstraints.HORIZONTAL;
+
+    LineGraphView graphView2 = new LineGraphView("Aggregate list", 380, 200, graphCons2, "Minutes", "Objects / Aggregates", false);
+    double[] aggrCreatedSeries = new double[AGGR_OBJ_HISTORY-1];
+    double[] avgSizeSeries = new double[AGGR_OBJ_HISTORY-1];
+    double[] timeSeries2 = new double[AGGR_OBJ_HISTORY-1];
+    for (int i=0; i<AGGR_OBJ_HISTORY-1; i++) {
+      int aggrsCreated = aggregatesHistory[(aggrObjPtr+i+1)%AGGR_OBJ_HISTORY]-aggregatesHistory[(aggrObjPtr+i)%AGGR_OBJ_HISTORY];
+      int objsCreated = objectsHistory[(aggrObjPtr+i+1)%AGGR_OBJ_HISTORY]-objectsHistory[(aggrObjPtr+i)%AGGR_OBJ_HISTORY];
+      aggrCreatedSeries[i] = aggrsCreated;
+      avgSizeSeries[i] = (aggrsCreated>0) ? (((double)objsCreated)/aggrsCreated) : 0;
+      timeSeries2[i] = (i*UPDATE_TIME*AGGR_OBJ_MULTIPLIER)/60000;
+    }
+    graphView2.addSeries("Aggregates created", timeSeries2, aggrCreatedSeries, Color.red);
+    graphView2.addSeries("Avg objects/aggregate", timeSeries2, avgSizeSeries, Color.green);
+      
+    summaryPanel.addDataView(aggregationView);
+    summaryPanel.addDataView(graphView);
+    summaryPanel.addDataView(graphView2);
+
+    return summaryPanel;
+  }
+  
+  protected synchronized void updateData() {
+    waitingHistory[waitingPtr] = aggregation.getNumObjectsWaiting();
+    waitingPtr = (waitingPtr+1) % WAITING_HISTORY;
+
+    if (((++seqNo) % AGGR_OBJ_MULTIPLIER) == 0) {
+      aggregatesHistory[aggrObjPtr] = aggregation.getNumAggregates();
+      objectsHistory[aggrObjPtr] = aggregation.getNumObjectsInAggregates();
+      aggrObjPtr = (aggrObjPtr+1) % AGGR_OBJ_HISTORY;
+    }
+  }
+}
--- pastry/src/rice/visualization/server/GlacierPanelCreator.java
+++ pastry/src/rice/visualization/server/GlacierPanelCreator.java
@@ -2,160 +2,93 @@ package rice.visualization.server;
 
 import rice.visualization.data.*;
 import rice.pastry.*;
-import rice.p2p.glacier.v1.*;
+import rice.p2p.glacier.v2.*;
 import rice.persistence.*;
 import rice.Continuation.*;
 
 import java.awt.*;
 import java.util.*;
 
-public class GlacierPanelCreator implements PanelCreator {
-  
-  public static int UPDATE_TIME = 1000;
-  
-  Vector data = new Vector();
+public class GlacierPanelCreator implements PanelCreator, GlacierStatisticsListener {
 
-  Vector cache = new Vector();
+  private static final int HISTORY = 300;
   
-  Vector times = new Vector();
+  GlacierImpl glacier;
+  GlacierStatistics history[] = new GlacierStatistics[HISTORY];
+  int msgTotal[] = new int[20];
+  int msgSent = 0;
+  int historyPtr = 0;
   
-  protected StorageManager manager;
-  
-  public GlacierPanelCreator() {
-    Thread t = new Thread("Glacier Panel Monitor Thread") {
-      public void run() {
-        while (true) {
-          try {
-            updateData();
-            Thread.currentThread().sleep(UPDATE_TIME);
-          } catch (InterruptedException e) {
-          }
-        }
-      }
-    };
-      
-    t.start();
+  public GlacierPanelCreator(GlacierImpl glacier) {
+    this.glacier = glacier;
+    glacier.addStatisticsListener(this);
+    Arrays.fill(msgTotal, 0);
+    for (int i=0; i<HISTORY; i++)
+      history[i] = null;
   }
   
   public DataPanel createPanel(Object[] objects) {
-    PastryNode node = null;
-    GlacierImpl glacier = null;
-    
-    for (int i=0; i<objects.length; i++) {
-      if (objects[i] instanceof PastryNode)
-        node = (PastryNode) objects[i];
-      else if (objects[i] instanceof rice.p2p.glacier.v1.GlacierImpl)
-        glacier = (GlacierImpl) objects[i];
-      else if (objects[i] instanceof StorageManager)
-        manager = (StorageManager) objects[i];
-      
-      if ((node != null) && (glacier != null) && (manager != null))
-        return createPanel(node, glacier, manager);
-    }
-    
-    return null;
-  }
-  
-  protected DataPanel createPanel(PastryNode node, GlacierImpl glacier, StorageManager manager) {
 
     DataPanel glacierPanel = new DataPanel("Glacier");
 
-/*
-    GlacierState state = glacier.getState();
-    int numFragments = glacier.getNumFragments();
-    
     GridBagConstraints glacierCons = new GridBagConstraints();
     glacierCons.gridx = 0;
     glacierCons.gridy = 0;
     glacierCons.fill = GridBagConstraints.HORIZONTAL;
     
     KeyValueListView glacierView = new KeyValueListView("Glacier Overview", 380, 200, glacierCons);
-    glacierView.add("Last SC", ""+state.lastStatusCast);
-    glacierView.add("Seq#", ""+state.currentSequenceNo);
-    glacierView.add("Holders", ""+state.holderList.size());
-    glacierView.add("Files", ""+state.fileList.size());
-    glacierView.add("History", ""+state.history.size()+" entries");
-    glacierView.add("Local time", ""+(new Date()));
-    glacierView.add("Next SC", ""+glacier.getNextStatusCastDate());
-    
-    int numOwn = 0, numCertain = 0, numProbable = 0, numDead = 0, numDuplicates = 0;
-    double[] fragmentHisto = new double[numFragments + 10];
-    double[] timeValues = new double[numFragments + 10];
-    for (int i=0; i<(numFragments+10); i++) {
-        fragmentHisto[i] = 0;
-        timeValues[i] = i;
-    }
-    
-    Enumeration enu = state.fileList.elements();
-    while (enu.hasMoreElements()) {
-        FileInfo fileInfo = (FileInfo)enu.nextElement();
-        int liveFragments = 0;
-        for (int i=0; i<numFragments; i++) {
-            boolean certainKnown = false;
-            for (int j=0; j<FileInfo.maxHoldersPerFragment; j++) {
-                if (fileInfo.holderKnown[i][j]) {
-                    if (fileInfo.holderCertain[i][j]) {
-                        if (!fileInfo.holderDead[i][j]) {
-                            if (certainKnown)
-                                numDuplicates ++;
-                            certainKnown = true;
-                            if (fileInfo.holderId[i][j] == null)
-                                numOwn ++;
-                            else
-                                numCertain ++;
-                        } else numDead ++;
-                    } else {
-                        if (fileInfo.holderDead[i][j])
-                            numDead ++;
-                        else
-                            numProbable ++;
-                    }
-                }
-            }
-            
-            if (certainKnown)
-                liveFragments ++;
-        }
-        
-        if (liveFragments > (numFragments+9))
-            liveFragments = numFragments+9;
-            
-        fragmentHisto[liveFragments] ++;
+    GlacierStatistics current = history[(historyPtr + HISTORY - 1) % HISTORY];
+    if (current != null) {
+      glacierView.add("Pending req", "" + current.pendingRequests);
+      glacierView.add("Neighbors", "" + current.numNeighbors);
+      glacierView.add("Fragments", "" + current.numFragments);
+      glacierView.add("Continuations", "" + current.numContinuations);
+      glacierView.add("Objs in trash", "" + current.numObjectsInTrash);
+      glacierView.add("Msg sent", "" + msgSent);
+    } else {
+      glacierView.add("Starting up...", "");
     }
 
-    GridBagConstraints glacierCons2 = new GridBagConstraints();
-    glacierCons2.gridx = 1;
-    glacierCons2.gridy = 0;
-    glacierCons2.fill = GridBagConstraints.HORIZONTAL;
+    GridBagConstraints countCons = new GridBagConstraints();
+    countCons.gridx = 1;
+    countCons.gridy = 0;
+    countCons.fill = GridBagConstraints.HORIZONTAL;
+    KeyValueListView countView = new KeyValueListView("Message count", 380, 200, countCons);
 
-    KeyValueListView glacierView2 = new KeyValueListView("Glacier Details", 380, 200, glacierCons2);
-    glacierView2.add("F.Local", ""+numOwn);
-    glacierView2.add("F.Certain", ""+numCertain);
-    glacierView2.add("F.Uncertain", ""+numProbable);
-    glacierView2.add("F.Dead", ""+numDead);
-    glacierView2.add("F.Duplicate", ""+numDuplicates);
-    glacierView.add("Files @"+numFragments, ""+((int)fragmentHisto[numFragments]));
+    GridBagConstraints graphCons = new GridBagConstraints();
+    graphCons.gridx = 2;
+    graphCons.gridy = 0;
+    graphCons.fill = GridBagConstraints.HORIZONTAL;
+    LineGraphView graphView = new LineGraphView("Messages", 380, 200, graphCons, "Minutes", "Messages", false);
 
-    GridBagConstraints fragmentHistoCons = new GridBagConstraints();
-    fragmentHistoCons.gridx = 2;
-    fragmentHistoCons.gridy = 0;
-    fragmentHistoCons.fill = GridBagConstraints.HORIZONTAL;
-
-    LineGraphView fragmentHistoView = new LineGraphView("Fragment histogram", 380, 200, fragmentHistoCons, "# Live fragments", "Files", false);
-    fragmentHisto[numFragments] = 0;
-    fragmentHistoView.addSeries("Data Stored", timeValues, fragmentHisto, Color.green);
+    String tagToString[] = new String[] { "--", "Neighbor", "Sync", "SyncManifests", "SyncFetch", "Handoff", "Debug", "Refresh", "Insert", "LookupHandles", "Lookup", "Fetch", "LocalScan" };
+    Color tagToColor[] = new Color[] { Color.white, Color.green, Color.blue, Color.black, Color.cyan, Color.darkGray, Color.lightGray, Color.magenta, Color.orange, Color.pink, Color.red, Color.yellow, Color.gray };
+    double[] timeSeries = new double[HISTORY];
+    for (int i=1; i<=12; i++) {
+      countView.add(tagToString[i], (current == null) ? "?" : ""+current.messagesSentByTag[i]+" ("+msgTotal[i]+" total)");
+      double[] countSeries = new double[HISTORY];
+      for (int j=0; j<HISTORY; j++) {
+        GlacierStatistics statJ = history[(historyPtr+j) % HISTORY];
+        timeSeries[j] = j;
+        countSeries[j] = (statJ == null) ? 0 : statJ.messagesSentByTag[i];
+      }
+      
+      graphView.addSeries("Tag "+i, timeSeries, countSeries, tagToColor[i]);
+    }
       
     glacierPanel.addDataView(glacierView);
-    glacierPanel.addDataView(glacierView2);
-    glacierPanel.addDataView(fragmentHistoView);
+    glacierPanel.addDataView(countView);
+    glacierPanel.addDataView(graphView);
 
-*/        
     return glacierPanel;
   }
   
-  protected synchronized void updateData() {
-    if (manager != null) {
-      /* do nothing */
+  public void receiveStatistics(GlacierStatistics stat) {
+    history[historyPtr] = stat;
+    historyPtr = (historyPtr+1) % HISTORY;
+    for (int i=0; i<stat.messagesSentByTag.length; i++) {
+      msgTotal[i] += stat.messagesSentByTag[i];
+      msgSent += stat.messagesSentByTag[i];
     }
   }
 }
