b2cf65414d9f117495226ed78d391b79a81e9f32
amislove <amislove@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-08-04 17:19:48 +0000

    *** empty log message ***
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@1917 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/post/messaging/RefreshMessage.java
@@ -0,0 +1,13 @@
+package rice.post.messaging;
+
+import java.io.*;
+
+import rice.p2p.commonapi.*;
+
+/**
+ * This is a message  reminding POST to refresh all data items stored
+ * in GCPast
+ */
+public class RefreshMessage implements Message {
+  
+}
