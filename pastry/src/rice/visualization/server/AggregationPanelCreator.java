5baf4cef61fed5afc10a70cebbe793959a12c806
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-08-18 23:37:09 +0000

    - Restructured; added two new graphs
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2009 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/visualization/server/AggregationPanelCreator.java
+++ pastry/src/rice/visualization/server/AggregationPanelCreator.java
@@ -12,28 +12,30 @@ import java.util.*;
 
 public class AggregationPanelCreator implements PanelCreator {
   
-  public static final int UPDATE_TIME = 5000;
-  public static final int AGGR_OBJ_MULTIPLIER = 12;
-  public static final int WAITING_HISTORY = 240;
-  public static final int AGGR_OBJ_HISTORY = 240;
+  public static final long SECONDS = 1000;
+  public static final long MINUTES = 60 * SECONDS;
+  public static final long HOURS = 60 * MINUTES;
+  public static final long DAYS = 24 * HOURS;
   
-  int[] waitingHistory = new int[WAITING_HISTORY];
-  int[] aggregatesHistory = new int[AGGR_OBJ_HISTORY];
-  int[] objectsHistory = new int[AGGR_OBJ_HISTORY];
-  int waitingPtr = 0, aggrObjPtr = 0, seqNo = 0;
+  public static final long UPDATE_TIME = 5 * SECONDS;
+  public static final long STATS_GRANULARITY = 10 * MINUTES;
+  public static final long STATS_RANGE = 2 * DAYS;
+  public static final int STATS_HISTORY = (int)(STATS_RANGE / STATS_GRANULARITY);
+  public static final int STATS_SUBSAMPLE = (int)(STATS_GRANULARITY / UPDATE_TIME);
+  
+  AggregationStatistics[] statsHistory = new AggregationStatistics[STATS_HISTORY];
+  AggregationStatistics lastStats = null;
+  int statsPtr = 0, statsTotal = 0, statsCounter = 0;
   AggregationImpl aggregation;
   
   public AggregationPanelCreator(rice.selector.Timer timer, AggregationImpl aggregation) {
     this.aggregation = aggregation;
 
-    Arrays.fill(aggregatesHistory, aggregation.getNumAggregates());
-    Arrays.fill(objectsHistory, aggregation.getNumObjectsInAggregates());
-
     timer.scheduleAtFixedRate(new rice.selector.TimerTask() {
       public void run() {
         updateData();
       }
-    }, UPDATE_TIME, UPDATE_TIME);
+    }, (int)UPDATE_TIME, (int)UPDATE_TIME);
   }
   
   public DataPanel createPanel(Object[] objects) {
@@ -47,8 +49,16 @@ public class AggregationPanelCreator implements PanelCreator {
     
     KeyValueListView aggregationView = new KeyValueListView("Aggregation Overview", 380, 200, cons);
     aggregationView.add("Obj. waiting", ""+aggregation.getNumObjectsWaiting());
-    aggregationView.add("Aggr. total", ""+aggregation.getNumAggregates());
-    aggregationView.add("Obj. in aggr", ""+aggregation.getNumObjectsInAggregates());
+    if (lastStats != null) {
+      aggregationView.add("Aggregates", lastStats.numAggregatesTotal+" ("+lastStats.numPointerArrays+"P "+lastStats.criticalAggregates+"C "+lastStats.orphanedAggregates+"O)");
+      aggregationView.add("Obj. total", lastStats.numObjectsTotal+" ("+lastStats.totalObjectsSize+" bytes)");
+      aggregationView.add("Obj. alive", lastStats.numObjectsAlive+" ("+lastStats.liveObjectsSize+" bytes)");
+
+      int numRealAggregates = lastStats.numAggregatesTotal - lastStats.numPointerArrays;
+      if (numRealAggregates > 0)
+        aggregationView.add("Aggr. factor", ""+(((float)lastStats.numObjectsAlive) / numRealAggregates));
+    }
+
     aggregationView.add("Obj. store", aggregation.getObjectStore().getClass().getName());
     aggregationView.add("Aggr. store", aggregation.getAggregateStore().getClass().getName());
     
@@ -60,33 +70,44 @@ public class AggregationPanelCreator implements PanelCreator {
     graphCons.gridy = 0;
     graphCons.fill = GridBagConstraints.HORIZONTAL;
 
-    LineGraphView graphView = new LineGraphView("Waiting list", 380, 200, graphCons, "Seconds", "Objects", false, false);
-    double[] waitingSeries = new double[WAITING_HISTORY];
-    double[] timeSeries = new double[WAITING_HISTORY];
-    for (int i=0; i<WAITING_HISTORY; i++) {
-      waitingSeries[i] = waitingHistory[(waitingPtr+i) % WAITING_HISTORY];
-      timeSeries[i] = (i*UPDATE_TIME)/1000;
+    LineGraphView graphView = new LineGraphView("Lifetime", 380, 200, graphCons, "Hours from now", "Occurrences", false, false);
+    if (lastStats != null) {
+      double[] objSeries = new double[lastStats.objectLifetimeHisto.length];
+      double[] aggrSeries = new double[lastStats.objectLifetimeHisto.length];
+      double[] timeSeries = new double[lastStats.objectLifetimeHisto.length];
+      for (int i=0; i<lastStats.objectLifetimeHisto.length; i++) {
+        objSeries[i] = lastStats.objectLifetimeHisto[i];
+        aggrSeries[i] = lastStats.aggregateLifetimeHisto[i];
+        timeSeries[i] = ((double)(i*lastStats.granularity)) / HOURS;
+      }
+        
+      graphView.addSeries("Objects", timeSeries, objSeries, Color.green);
+      graphView.addSeries("Aggregates", timeSeries, aggrSeries, Color.blue);
     }
-    graphView.addSeries("Waiting", timeSeries, waitingSeries, Color.blue);
 
     GridBagConstraints graphCons2 = new GridBagConstraints();
     graphCons2.gridx = 2;
     graphCons2.gridy = 0;
     graphCons2.fill = GridBagConstraints.HORIZONTAL;
 
-    LineGraphView graphView2 = new LineGraphView("Aggregate list", 380, 200, graphCons2, "Minutes", "Objects / Aggregates", false, false);
-    double[] aggrCreatedSeries = new double[AGGR_OBJ_HISTORY-1];
-    double[] avgSizeSeries = new double[AGGR_OBJ_HISTORY-1];
-    double[] timeSeries2 = new double[AGGR_OBJ_HISTORY-1];
-    for (int i=0; i<AGGR_OBJ_HISTORY-1; i++) {
-      int aggrsCreated = aggregatesHistory[(aggrObjPtr+i+1)%AGGR_OBJ_HISTORY]-aggregatesHistory[(aggrObjPtr+i)%AGGR_OBJ_HISTORY];
-      int objsCreated = objectsHistory[(aggrObjPtr+i+1)%AGGR_OBJ_HISTORY]-objectsHistory[(aggrObjPtr+i)%AGGR_OBJ_HISTORY];
-      aggrCreatedSeries[i] = aggrsCreated;
-      avgSizeSeries[i] = (aggrsCreated>0) ? (((double)objsCreated)/aggrsCreated) : 0;
-      timeSeries2[i] = (i*UPDATE_TIME*AGGR_OBJ_MULTIPLIER)/60000;
+    LineGraphView graphView2 = new LineGraphView("Aggregate list", 380, 200, graphCons2, "Hours", "Items", false, false);
+    if (statsTotal > 0) {
+      double[] objTotalSeries = new double[statsTotal];
+      double[] objAliveSeries = new double[statsTotal];
+      double[] aggrTotalSeries = new double[statsTotal];
+      double[] timeSeries = new double[statsTotal];
+      for (int i=0; i<statsTotal; i++) {
+        AggregationStatistics thisStats = statsHistory[(statsPtr + 2*STATS_HISTORY - (1+i))%STATS_HISTORY];
+        objTotalSeries[i] = thisStats.numObjectsTotal;
+        objAliveSeries[i] = thisStats.numObjectsAlive;
+        aggrTotalSeries[i] = thisStats.numAggregatesTotal;
+        timeSeries[i] = (statsTotal - (1+i)) * (((double)(STATS_SUBSAMPLE * UPDATE_TIME))/HOURS);
+      }
+      
+      graphView2.addSeries("Objects total", timeSeries, objTotalSeries, Color.red);
+      graphView2.addSeries("Objects alive", timeSeries, objAliveSeries, Color.green);
+      graphView2.addSeries("Aggregates", timeSeries, aggrTotalSeries, Color.blue);
     }
-    graphView2.addSeries("Aggregates created", timeSeries2, aggrCreatedSeries, Color.red);
-    graphView2.addSeries("Avg objects/aggregate", timeSeries2, avgSizeSeries, Color.green);
       
     summaryPanel.addDataView(aggregationView);
     summaryPanel.addDataView(graphView);
@@ -96,13 +117,13 @@ public class AggregationPanelCreator implements PanelCreator {
   }
   
   protected synchronized void updateData() {
-    waitingHistory[waitingPtr] = aggregation.getNumObjectsWaiting();
-    waitingPtr = (waitingPtr+1) % WAITING_HISTORY;
-
-    if (((++seqNo) % AGGR_OBJ_MULTIPLIER) == 0) {
-      aggregatesHistory[aggrObjPtr] = aggregation.getNumAggregates();
-      objectsHistory[aggrObjPtr] = aggregation.getNumObjectsInAggregates();
-      aggrObjPtr = (aggrObjPtr+1) % AGGR_OBJ_HISTORY;
+    lastStats = aggregation.getStatistics();
+    if ((--statsCounter) <= 0) {
+      statsHistory[statsPtr] = lastStats;
+      statsPtr = (statsPtr+1) % STATS_HISTORY;
+      if (statsTotal < STATS_HISTORY)
+        statsTotal ++;
+      statsCounter = STATS_SUBSAMPLE;
     }
   }
 }
