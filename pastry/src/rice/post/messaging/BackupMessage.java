7e90a79d5410be4a3307041133259bdec3d64714
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-09-10 19:24:04 +0000

    - Added a reminder message for log head backups
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2056 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/post/messaging/BackupMessage.java
@@ -0,0 +1,12 @@
+package rice.post.messaging;
+
+import java.io.*;
+
+import rice.p2p.commonapi.*;
+
+/**
+ * This is a message reminding POST to backup its log heads
+ */
+public class BackupMessage implements Message {
+  
+}
