06809e3e84ff78bfc728f7610d626120cbc70b5c
ahae <ahae@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2004-10-07 19:58:36 +0000

    - Remove old log head backups with infinity timeout
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2087 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/p2p/glacier/v2/GlacierImpl.java
+++ pastry/src/rice/p2p/glacier/v2/GlacierImpl.java
@@ -579,7 +579,9 @@ public class GlacierImpl implements Glacier, Past, GCPast, VersioningPast, Appli
           final Id thisKey = (Id) iter.next();
           final FragmentMetadata metadata = (FragmentMetadata) fragmentStorage.getMetadata(thisKey);
           if (metadata != null) {
-            if (metadata.getCurrentExpiration() < now) {
+            if ((metadata.getCurrentExpiration() < now) || /* delete old log head backups with infinity expiration */
+                ((((FragmentKey)thisKey).getVersionKey().getVersion() != 0) && (metadata.getCurrentExpiration() == GCPast.INFINITY_EXPIRATION)))
+            {
               candidates ++;
               if (doneSoFar < garbageCollectionMaxFragmentsPerRun) {
                 doneSoFar ++;
