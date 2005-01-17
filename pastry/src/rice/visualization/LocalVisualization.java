b5094e5d87fa9946b88dc47ede42240c721053bd
amislove <amislove@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2005-01-17 05:45:26 +0000

    added local vis via panel
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2130 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/visualization/LocalVisualization.java
@@ -0,0 +1,61 @@
+package rice.visualization;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import rice.pastry.NodeId;
+import rice.pastry.dist.DistNodeHandle;
+import rice.visualization.client.*;
+import rice.visualization.data.Data;
+import rice.visualization.data.DataProvider;
+
+public class LocalVisualization implements DataProvider {
+  
+  public static int PORT_OFFSET = 3847;
+  public static int REFRESH_TIME = 1000;
+  
+  protected LocalVisualizationFrame frame;
+  
+  protected DistNodeHandle handle;
+  
+  protected Data data;
+  
+  protected VisualizationClient client;
+
+  public LocalVisualization(DistNodeHandle handle) {
+    this.handle = handle;
+    this.frame = new LocalVisualizationFrame(this);
+    
+    Thread t = new Thread() {
+      public void run() {
+        try {
+          while (true) {
+            Thread.currentThread().sleep(REFRESH_TIME);
+            updateData();
+            frame.repaint();
+          }
+        } catch (Exception e) {
+          System.out.println(e);
+        }
+      }
+    };
+    
+    t.start(); 
+  }
+  
+  public Data getData() {
+    return data;
+  }
+  
+  protected void updateData() {
+    if (client == null) {
+      InetSocketAddress address = new InetSocketAddress(handle.getAddress().getAddress(), handle.getAddress().getPort() + PORT_OFFSET);
+      client = new VisualizationClient(address);
+      client.connect();
+      frame.nodeSelected(new Node(null, null), client.getData());
+    }
+        
+    this.data = client.getData();
+  }
+}
