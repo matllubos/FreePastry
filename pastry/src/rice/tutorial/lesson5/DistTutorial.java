085fb9837fa755ccfcf7beac9ec187d65bdae06f
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2005-05-09 16:57:12 +0000

    initial checkin of lesson5
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2440 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/tutorial/lesson5/DistTutorial.java
@@ -0,0 +1,80 @@
+package rice.tutorial.lesson5;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+import rice.p2p.commonapi.Id;
+import rice.p2p.commonapi.NodeHandleSet;
+import rice.pastry.NodeHandle;
+import rice.pastry.NodeIdFactory;
+import rice.pastry.PastryNode;
+import rice.pastry.PastryNodeFactory;
+import rice.pastry.leafset.LeafSet;
+import rice.pastry.socket.SocketPastryNodeFactory;
+import rice.pastry.standard.RandomNodeIdFactory;
+
+/**
+ * This tutorial shows how to setup a FreePastry node using the Socket Protocol.
+ * 
+ * @author Jeff Hoye
+ */
+public class DistTutorial {
+
+  /**
+   * This constructor sets up a PastryNode.  It will bootstrap to an 
+   * existing ring if it can find one at the specified location, otherwise
+   * it will start a new ring.
+   * 
+   * @param bindport the local port to bind to 
+   * @param bootaddress the IP:port of the node to boot from
+   */
+  public DistTutorial(int bindport, InetSocketAddress bootaddress) throws Exception {
+    
+    // Generate the NodeIds Randomly
+    NodeIdFactory nidFactory = new RandomNodeIdFactory();
+    
+    // construct the PastryNodeFactory, this is how we use rice.pastry.socket
+    PastryNodeFactory factory = new SocketPastryNodeFactory(nidFactory, bindport);
+
+    // This will return null if we there is no node at that location
+    NodeHandle bootHandle = ((SocketPastryNodeFactory)factory).getNodeHandle(bootaddress);
+
+    // construct a node, passing the null boothandle on the first loop will cause the node to start its own ring
+    PastryNode node = factory.newNode(bootHandle);
+      
+    // the node may require sending several messages to fully boot into the ring
+    while(!node.isReady()) {
+      // delay so we don't busy-wait
+      Thread.sleep(100);
+    }
+    
+    System.out.println("Finished creating new node "+node);
+    
+    // construct a new MyApp
+    MyApp app = new MyApp(node);
+    
+    // wait 15 seconds
+    Thread.sleep(15000);
+    
+    // cancel the task
+    app.cancelTask();
+  }
+
+  /**
+   * Usage: 
+   * java [-cp FreePastry-<version>.jar] rice.tutorial.DistTutorial localbindport bootIP bootPort
+   * example java rice.tutorial.DistTutorial 9001 pokey.cs.almamater.edu 9001
+   */
+  public static void main(String[] args) throws Exception {
+    // the port to use locally
+    int bindport = Integer.parseInt(args[0]);
+    
+    // build the bootaddress from the command line args
+    InetAddress bootaddr = InetAddress.getByName(args[1]);
+    int bootport = Integer.parseInt(args[2]);
+    InetSocketAddress bootaddress = new InetSocketAddress(bootaddr,bootport);
+
+    // launch our node!
+    DistTutorial dt = new DistTutorial(bindport, bootaddress);
+  }
+}
---
+++ pastry/src/rice/tutorial/lesson5/MyApp.java
@@ -0,0 +1,121 @@
+/*
+ * Created on Apr 15, 2005
+ */
+package rice.tutorial.lesson5;
+
+import rice.p2p.commonapi.Application;
+import rice.p2p.commonapi.CancellableTask;
+import rice.p2p.commonapi.Endpoint;
+import rice.p2p.commonapi.Id;
+import rice.p2p.commonapi.Message;
+import rice.p2p.commonapi.Node;
+import rice.p2p.commonapi.NodeHandle;
+import rice.p2p.commonapi.RouteMessage;
+import rice.tutorial.lesson3.MyMsg;
+
+/**
+ * This app shows how to trigger regularly scheduled
+ * events on the FreePastry thread.
+ * 
+ * @author Jeff Hoye
+ */
+public class MyApp implements Application {
+  
+  /**
+   * This is the task sends the MessageToSelf every 5 
+   * seconds.  We keep it as a member variable to 
+   * cancel later.
+   */
+  CancellableTask messageToSelfTask;
+  
+  /**
+   * The Endpoint represents the underlieing node.  By making calls on the 
+   * Endpoint, it assures that the message will be delivered to a MyApp on whichever
+   * node the message is intended for.
+   */
+  protected Endpoint endpoint;
+
+  /**
+   * NOTE: It is unsafe to send this message to 
+   * anyone but the localnode.  
+   * 
+   * The reason:
+   * This is an inner class of MyApp, and therefore
+   * has an automatic reference to
+   * MyApp which is not serializable!  Do not 
+   * attempt to send over the wire, you will get a 
+   * NotSerializableException!
+   */
+  class MessageToSelf implements Message {
+    public int getPriority() {
+      return 0;
+    }    
+  }
+  
+  public MyApp(Node node) {
+    // We are only going to use one instance of this application on each PastryNode
+    this.endpoint = node.registerApplication(this, "myinstance");
+    // Send MessageToSelf every 5 seconds, starting in 3 seconds
+    messageToSelfTask = endpoint.scheduleMessage(new MessageToSelf(), 3000, 5000);
+  }
+  
+  /**
+   * Call this to cancel the task.
+   */
+  public void cancelTask() {
+    messageToSelfTask.cancel(); 
+  }
+
+  
+  
+  /**
+   * Called to route a message to the id
+   */
+  public void routeMyMsg(Id id) {
+    System.out.println(this+" sending to "+id);    
+    Message msg = new MyMsg(endpoint.getId(), id);
+    endpoint.route(id, msg, null);
+  }
+  
+  /**
+   * Called to directly send a message to the nh
+   */
+  public void routeMyMsgDirect(NodeHandle nh) {
+    System.out.println(this+" sending direct to "+nh);    
+    Message msg = new MyMsg(endpoint.getId(), nh.getId());
+    endpoint.route(null, msg, nh);
+  }
+    
+  /**
+   * Called when we receive a message.
+   */
+  public void deliver(Id id, Message message) {
+    System.out.println(this+" received "+message);
+    if (message instanceof MessageToSelf) {
+      // This will get called every 5 seconds, on Pastry's thread.
+      // Thus now we can assume we are on Pastry's thread.
+      // TODO: whatever... send messages to other nodes? print out status?
+      System.out.println("I got the MessageToSelf at time:"+System.currentTimeMillis());
+    }
+  }
+
+  /**
+   * Called when you hear about a new neighbor.
+   * Don't worry about this method for now.
+   */
+  public void update(NodeHandle handle, boolean joined) {
+  }
+  
+  /**
+   * Called a message travels along your path.
+   * Don't worry about this method for now.
+   */
+  public boolean forward(RouteMessage message) {
+    return true;
+  }
+  
+  public String toString() {
+    return "MyApp "+endpoint.getId();
+  }
+
+}
