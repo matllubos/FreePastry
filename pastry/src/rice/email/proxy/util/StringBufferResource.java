3dee0b81eeb2304140647817caa17a2b2c3a0ee0
amislove <amislove@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2005-05-23 03:38:31 +0000

    overhauled StringBufferResource to be more memory efficient
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2484 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/email/proxy/util/StringBufferResource.java
+++ pastry/src/rice/email/proxy/util/StringBufferResource.java
@@ -6,46 +6,64 @@ public class StringBufferResource implements Resource {
   
   public static int MAXIMUM_LENGTH = 7000000;
   
-  StringWriter _currentWriter;
-  StringBuffer _contentBuffer;
+  /**
+   * The buffers used in the resource 
+   */
+  protected StringWriter writer;
+  protected StringBuffer buffer;
   
   public StringBufferResource() {
   }
   
-  public String toString() {
-    return _contentBuffer.toString();
-  }
-  
   public StringBufferResource(String initalValue) {
-    _contentBuffer = new StringBuffer(initalValue);
+    buffer = new StringBuffer(initalValue);
   }
   
   public Writer getWriter() throws IOException {
-//    _currentWriter = new LimitedStringWriter(MAXIMUM_LENGTH);
-    _currentWriter = new StringWriter();
+//    writer = new LimitedStringWriter(MAXIMUM_LENGTH);
+    writer = new StringWriter();
     
-    return _currentWriter;
+    return writer;
   }
   
   public InputStream getInputStream() throws IOException {
     closeInput();
     
-    return new ByteArrayInputStream(_contentBuffer.toString().getBytes());
+    return new InputStream() {
+      int pos = 0;
+      
+      public int read() {
+        return (pos < buffer.length() ? (int) buffer.charAt(pos++) & 0xFF : 0);
+      }
+      
+      public int read(byte[] buf, int off, int len) {
+        if (pos >= buffer.length())
+          return 0;
+        
+        if (len > buffer.length() - pos)
+          len = buffer.length() - pos;
+        
+        for (int i=0; i<len; i++)
+          buf[off+i] = (byte) buffer.charAt(pos++);
+        
+        return len;
+      }
+    };
   }
   
   public Reader getReader() throws IOException {
     closeInput();
     
-    return new StringReader(_contentBuffer.toString());
+    return new InputStreamReader(getInputStream());
   }
   
   private void closeInput() throws IOException {
-    if (_currentWriter != null) {
-      _contentBuffer = _currentWriter.getBuffer();
-      _currentWriter = null;
+    if (writer != null) {
+      buffer = writer.getBuffer();
+      writer = null;
     }
     
-    if (_contentBuffer == null)
+    if (buffer == null)
       throw new IOException("No content has been written");
   }
   
@@ -56,11 +74,11 @@ public class StringBufferResource implements Resource {
       return 0;
     }
     
-    return _contentBuffer.toString().length();
+    return buffer.length();
   }
   
   public void delete() {
-    _contentBuffer = null;
-    _currentWriter = null;
+    buffer = null;
+    writer = null;
   }
 }
