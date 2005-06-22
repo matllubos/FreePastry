598899f903e6a759c03df67c238ae13a18e9f3c0
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2005-06-22 20:53:43 +0000

    Renamed to ParamsNotPresentException, which is Runtime, not IOException
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2590 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/environment/params/simple/ParamsNotPresentException.java
+++ pastry/src/rice/environment/params/simple/ParamsNotPresentException.java
@@ -3,21 +3,20 @@
  */
 package rice.environment.params.simple;
 
-import java.io.IOException;
 
 /**
  * Unable to load the default parameters file.  This is required for running pastry.
  * 
  * @author Jeff Hoye
  */
-public class DefaultParamsNotPresentException extends IOException {
-  IOException subexception;
+public class ParamsNotPresentException extends RuntimeException {
+  Exception subexception;
   
   /**
    * @param ioe
    */
-  public DefaultParamsNotPresentException(IOException ioe) {
-    this.subexception = ioe;
+  public ParamsNotPresentException(Exception e) {
+    this.subexception = e;
   }
   
 }
