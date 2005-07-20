ff1468fafcde53412e5ead839ec0b98bd5551a5d
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2005-07-20 14:51:40 +0000

    Reduced chance of seeding 2 nodes with the same time.  It now takes into account the local InetAddress.
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2668 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/environment/random/simple/SimpleRandomSource.java
+++ pastry/src/rice/environment/random/simple/SimpleRandomSource.java
@@ -3,6 +3,7 @@
  */
 package rice.environment.random.simple;
 
+import java.net.InetAddress;
 import java.util.Random;
 
 import rice.environment.random.RandomSource;
@@ -14,14 +15,33 @@ public class SimpleRandomSource implements RandomSource {
   Random rnd;
   
   public SimpleRandomSource() {
-    rnd = new Random();
+    this(0);
   }
   
   public SimpleRandomSource(long seed) {
     if (seed != 0) {
       rnd = new Random(seed);
     } else {
-      rnd = new Random(); 
+      // NOTE: Since we are often starting up a bunch of nodes on planetlab
+      // at the same time, we need this randomsource to be seeded by more
+      // than just the clock, we will include the IP address
+      // as amazing as this sounds, it happened in a network of 20 on 7/19/2005
+      // also, if you think about it, I was starting all of the nodes at the same 
+      // instant, and they had synchronized clocks, if they all started within 1/10th of
+      // a second, then there is only 100 different numbers to seed the generator with
+      // -Jeff
+      long time = System.currentTimeMillis();
+      try {
+        byte[] foo = InetAddress.getLocalHost().getAddress();
+        for (int ctr = 0; ctr < foo.length; ctr++) {
+          int i = (int)foo[ctr];
+          i <<= (ctr*8);
+          time ^= i; 
+        }
+      } catch (Exception e) {
+        // if there is no NIC, screw it, this is really unlikely anyway  
+      }          
+      rnd = new Random(time); 
     }
   }
   
