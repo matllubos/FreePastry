f9f0cd5408ce576fec313321449a2b936e6e3f62
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2005-08-09 14:46:31 +0000

    various new features
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2710 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/pastry/testing/MemoryTest.java
+++ pastry/src/rice/pastry/testing/MemoryTest.java
@@ -28,6 +28,18 @@ public class MemoryTest {
    * The idea is to keep a ring of about 10 nodes alive, but one by one recycle the nodes out.  In the end, 
    * creating a whole lot of nodes, to see if they are being properly cleaned up.
    *
+   * TODO: make this return a boolean:
+   * 
+   * Record the memory at start (M1), then after creating the ringSize*2 nodes (M2)
+   * (M2-M1/ringSize) is the approx memory for 1 node.
+   * 
+   * M3 = (M2-M1) which is basically the amount of memory that our ring of 10 should take up.
+   * 
+   * at the end of the test, record M4.  
+   * 
+   * if M4 > (M1+2*M3) then we probably have a leak, fail.
+   * 
+   * TODO: test this with other environment settings
    */
   public static void testOneEnvironment() throws Exception {
     //System.setOut(new PrintStream(new FileOutputStream("memtest.txt")));
@@ -39,11 +51,19 @@ public class MemoryTest {
     
     LinkedList nodes = new LinkedList();
     Runtime run = Runtime.getRuntime();
-    System.out.println("Memory:"+run.freeMemory()+":"+run.totalMemory()+":"+run.maxMemory());
+    long memUsed = run.totalMemory()-run.freeMemory();
+    System.out.println("Memory:"+memUsed);
     Environment env = new Environment();    
+    env.getParameters().setBoolean("pastry_factory_selectorPerNode", false);
+    env.getParameters().setBoolean("pastry_factory_processorPerNode", false);
+    env.getParameters().setInt("pastry_socket_scm_ping_delay", 1000);
+    env.getParameters().setInt("pastry_socket_scm_ping_jitter", 500);
+    env.getParameters().setInt("pastry_socket_scm_num_ping_tries", 2);
+    
     NodeIdFactory nidFactory = new RandomNodeIdFactory(env);
     
-    InetAddress localAddress = InetAddress.getByName("139.19.64.79");
+    //InetAddress localAddress = InetAddress.getByName("139.19.64.79");
+    InetAddress localAddress = InetAddress.getLocalHost();
     
     PastryNodeFactory factory = new SocketPastryNodeFactory(nidFactory, localAddress, startPort, env);
 
@@ -65,7 +85,8 @@ public class MemoryTest {
       
       // print the current status
       long waitedFor = env.getTimeSource().currentTimeMillis() - waitTime;
-      System.out.println(curNode+"/"+numNodes+" Memory:"+run.freeMemory()+":"+run.totalMemory()+":"+run.maxMemory()+" leafset size:"+node.getLeafSet().size()+" "+node+" after "+waitedFor);      
+      memUsed = run.totalMemory()-run.freeMemory();
+      System.out.println(curNode+"/"+numNodes+" Memory:"+memUsed+" leafset size:"+node.getLeafSet().size()+" "+node+" after "+waitedFor);      
 
       // always boot off of the previous node
       bootaddress = ((SocketNodeHandle)node.getLocalHandle()).getAddress();
