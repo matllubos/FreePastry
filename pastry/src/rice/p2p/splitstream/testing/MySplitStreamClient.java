93af1810cebc5a98c03378b6560f7750edbbe1fe
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2005-09-14 08:27:54 +0000

    Now with failover of the publisher!
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2762 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/p2p/splitstream/testing/MySplitStreamClient.java
+++ pastry/src/rice/p2p/splitstream/testing/MySplitStreamClient.java
@@ -4,10 +4,12 @@
 package rice.p2p.splitstream.testing;
 
 import rice.p2p.commonapi.*;
-import rice.p2p.commonapi.Node;
+import rice.p2p.commonapi.Id;
+import rice.p2p.commonapi.IdRange;
 import rice.p2p.splitstream.*;
-import rice.p2p.splitstream.SplitStreamClient;
 import rice.p2p.util.MathUtils;
+import rice.pastry.*;
+import rice.pastry.leafset.RangeCannotBeDeterminedException;
 import rice.selector.TimerTask;
 
 /**
@@ -17,14 +19,14 @@ public class MySplitStreamClient implements SplitStreamClient {
 
   int SEND_PERIOD = 15000;//1000;
   
-  // 16KBps/base
-  int msgSize = 4;
+  // 160/8 id + 4 seq
+  int msgSize = 24;
   
   /**
    * The underlying common api node
    *  
    */
-  private Node n = null;
+  private PastryNode n = null;
 
   /**
    * The stripes for a channel
@@ -54,7 +56,7 @@ public class MySplitStreamClient implements SplitStreamClient {
   
   int curSeq = 0;
   
-  public MySplitStreamClient(Node n, String instance) {
+  public MySplitStreamClient(PastryNode n, String instance) {
     this.n = n;
     this.instance = instance;
     this.ss = new SplitStreamImpl(n, instance);
@@ -78,16 +80,31 @@ public class MySplitStreamClient implements SplitStreamClient {
     return stripes;
   }
 
-  public void publishNext() {
-    publish(curSeq);
-    curSeq++;
+  public boolean shouldPublish() {
+    try {
+      IdRange range = n.getLeafSet().range(n.getLocalHandle(), 0);
+     
+      return range.containsId(rice.pastry.Id.build());
+    } catch (RangeCannotBeDeterminedException rcbde) {
+      return true; 
+    }
   }
   
-  public void publish(int seq) {
-    System.out.println("MSSC.publish("+seq+"):"+n.getEnvironment().getTimeSource().currentTimeMillis());
+  public void publishNext() {
+    if (shouldPublish()) {
+      publish(n.getId(), curSeq);
+      curSeq++;
+    }
+  }
+  
+  public void publish(Id id, int seq) {
+    System.out.println("MSSC.publish("+id+":"+seq+"):"+n.getEnvironment().getTimeSource().currentTimeMillis());
     byte[] msg = new byte[msgSize];
     byte[] head = MathUtils.intToByteArray(seq);
     System.arraycopy(head, 0, msg, 0, 4);
+    byte[] idArray = id.toByteArray();
+    System.arraycopy(idArray, 0, msg, 4, 20);
+    rice.pastry.Id.build(idArray);
     publishAll(msg);
   }
   
@@ -109,9 +126,14 @@ public class MySplitStreamClient implements SplitStreamClient {
     byte[] theInt = new byte[4];
     System.arraycopy(data, 0, theInt, 0, 4);
     int seq = MathUtils.byteArrayToInt(theInt);
+    
+    byte[] material = new byte[20];
+    System.arraycopy(data, 4, material, 0, 20);
+    Id publisher = rice.pastry.Id.build(material);
+        
     Id stripeId = (rice.pastry.Id) (s.getStripeId().getId());
     String stripeStr = stripeId.toString().substring(3, 4);
-    System.out.println("deliver("+stripeStr+","+seq+"):"+n.getEnvironment().getTimeSource().currentTimeMillis());
+    System.out.println("deliver("+stripeStr+","+publisher+","+seq+"):"+n.getEnvironment().getTimeSource().currentTimeMillis());
   }
 
   /**
@@ -119,7 +141,7 @@ public class MySplitStreamClient implements SplitStreamClient {
    */
   public void startPublishTask() {
     publishTask = new TimerTask() {
-      public void run() {
+      public void run() {        
         publishNext();
       }
     };    
