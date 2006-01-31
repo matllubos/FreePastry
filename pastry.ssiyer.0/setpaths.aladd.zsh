ed657f8d6b0cc97a0d710358920ac18f4e3cc10b
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2006-01-31 18:33:10 +0000

    removed extremely old tags
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@2999 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry.ssiyer.0/setpaths.aladd.zsh
+++
@@ -1,5 +0,0 @@
-JAVA_DEV_ROOT=/home/aladd/code/rcode/pastry
-JAVA_HOME=/usr/lib/j2sdk1.3
-IS_UNIX=true
-CVSROOT=:ext:aladd@dosa.cs.rice.edu:/net/madras/FreePastry/cvsroot
-CVSRSH=ssh
--- pastry.ssiyer.0/shipdocs.csh
+++
@@ -1,4 +0,0 @@
-#!/bin/csh
-
-rm -r /home/aladd/public_html/pastry/docs
-cp -r /home/aladd/code/pastry/docs /home/aladd/public_html/pastry
