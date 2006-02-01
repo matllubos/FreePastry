abd97b336b2028c1b76ce500c7b57d03b002e911
jstewart <jstewart@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2006-02-01 16:10:54 +0000

    reformat for readability.
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@3005 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/pastry/ScheduledMessage.java
+++ pastry/src/rice/pastry/ScheduledMessage.java
@@ -1,4 +1,3 @@
-
 package rice.pastry;
 
 import rice.environment.logging.Logger;
@@ -7,65 +6,69 @@ import rice.selector.TimerTask;
 
 /**
  * A class that represents scheduled message events
- *
- * @version $Id$
- *
+ * 
+ * @version $Id: ScheduledMessage.java 2808 2005-11-22 15:38:49 +0100 (Tue, 22
+ *          Nov 2005) jeffh $
+ * 
  * @author Peter Druschel
  */
 public class ScheduledMessage extends TimerTask {
   private PastryNode localNode;
+
   private Message msg;
 
   /**
    * Constructor
-   *
-   * @param the message
+   * 
+   * @param the
+   *          message
    */
   public ScheduledMessage(PastryNode pn, Message msg) {
-      localNode = pn;
-      this.msg = msg;
+    localNode = pn;
+    this.msg = msg;
   }
 
   /**
    * Returns the message
-   *
+   * 
    * @return the message
    */
   public Message getMessage() {
-      return msg;
+    return msg;
   }
 
-	public PastryNode getLocalNode() {
-		return localNode;
-	}
+  public PastryNode getLocalNode() {
+    return localNode;
+  }
 
   /**
    * deliver the message
    */
   public void run() {
     try {
-    	// timing with cancellation
-    	Message m = msg;
-    	if (m != null)
-        localNode.receiveMessage(msg);
+      // timing with cancellation
+      Message m = msg;
+      if (m != null) localNode.receiveMessage(msg);
     } catch (Exception e) {
       Logger logger = localNode.getEnvironment().getLogManager().getLogger(getClass(), null);
       if (logger.level <= Logger.WARNING) logger.logException("Delivering " + this + " caused exception ", e);
     }
   }
 
-	public String toString() {
-		return "SchedMsg for "+msg;	
-	}
-  /* (non-Javadoc)
+  public String toString() {
+    return "SchedMsg for " + msg;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
    * @see rice.p2p.commonapi.CancellableTask#cancel()
    */
   public boolean cancel() {
-		// memory management
-		msg = null;
-		localNode = null;
+    // memory management
+    msg = null;
+    localNode = null;
     return super.cancel();
   }
 
 }
-
