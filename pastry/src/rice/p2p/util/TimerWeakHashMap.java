8b9596de0701c9c4c4a5d2b93dc9ba748a5b2697
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2006-06-22 11:57:11 +0000

    Added timer weak hash map which is a weak hash map that keeps elements for a minimum time when entered.
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@3308 c21dacaf-8602-0410-aacd-8bc41df2cbe7

---
+++ pastry/src/rice/p2p/util/TimerWeakHashMap.java
@@ -0,0 +1,51 @@
+/*
+ * Created on Jun 9, 2006
+ */
+package rice.p2p.util;
+
+import java.util.WeakHashMap;
+
+import rice.selector.*;
+
+/**
+ * Weak hash map that holds hard link to keys for a minimum time.
+ * 
+ * @author Jeff Hoye
+ */
+public class TimerWeakHashMap extends WeakHashMap {
+
+  int defaultDelay;
+  Timer timer;
+  
+  public TimerWeakHashMap(Timer t, int delay) {
+    this.defaultDelay = delay;
+    timer = t;
+  }
+  
+  static class HardLinkTimerTask extends TimerTask {
+    Object hardLink;
+    public HardLinkTimerTask(Object hardLink) {
+      this.hardLink = hardLink;
+    }
+    public void run() {
+      // do nothing, just expire, taking the hard link away with you
+    }
+  }
+
+  
+  public Object put(Object key, Object val) {
+    refresh(key);
+    return super.put(key, val);
+  }
+
+  public void refresh(Object key) {
+    refresh(key, defaultDelay);
+  }
+  
+  public void refresh(Object key, int delay) {      
+    timer.schedule(
+        new HardLinkTimerTask(key), delay);
+  }
+  
+  
+}
