df29e575d31bd8458b4369a9023e9a6a0b963b87
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2006-08-08 13:27:29 +0000

    made tran[] public so other things such as the routing table can use it
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@3347 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/pastry/Id.java
+++ pastry/src/rice/pastry/Id.java
@@ -31,7 +31,7 @@ public class Id implements rice.p2p.commonapi.Id {
   /**
    * The static translation array
    */
-  private static String tran[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+  public static final String tran[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
   
   /**
    * This is the bit length of the node ids. If it is n, then there are 2^n possible different Ids.
