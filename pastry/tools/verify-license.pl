e44d15de2d57f65e806efbf6ef5d4a422b5ef922
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2007-02-15 13:51:56 +0000

    added checks for dos files and tabs
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@3606 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/tools/verify-license.pl
+++ pastry/tools/verify-license.pl
@@ -16,7 +16,12 @@ $license_text = quotemeta($license_text); #escape the license
 #print "$license_text\n";
 
 our @needs_license;
-our $msg;
+our @has_tab;
+our @has_cr;
+
+our $msg_license = "";
+our $msg_tab = "";
+our $msg_cr = "";
 my $numFiles = 0;
 
 open FILEHANDLE, "find $src_dir \\\( -name '.svn' -prune \\\) -o \\\( -name '*.java' -print \\\) |";
@@ -26,19 +31,34 @@ while (<FILEHANDLE>) {
 #	 print "$java_file\n"; 
 	 $numFiles++;
 	 
-	 my $java_contents = `head -50 ./$java_file`;
+	 
+	 open my $fh, "<", $java_file;
+	 local $/;  # so we read whole file in one go
+	 my $java_contents = <$fh>;
+	 close $fh;
+#	 my $java_contents = `head -50 ./$java_file`;
 	 if ($java_contents =~ /^$license_text/) {
-#		 print "$java_file TRUE\n";
 	 } else {
-#		 print "$java_file FALSE\n";		 
-		 $msg .= "$java_file\n";
-     push @needs_license, $java_file;
+		 $msg_license .= "\t$java_file\n";
+         push @needs_license, $java_file;
+	 }
+	 
+	 if ($java_contents =~ /\t/) {
+		 $msg_tab .= "\t$java_file\n";
+         push @has_tab, $java_file;	 
+	 }
+	 
+	 if ($java_contents =~ /\r/) {
+		 $msg_cr .= "\t$java_file\n";
+         push @has_cr, $java_file;	 
 	 }
 }
 
 my $num_needs_license = scalar(@needs_license);
+my $num_has_tab = scalar(@has_tab);
+my $num_has_cr = scalar(@has_cr);
 
-if ($num_needs_license) {
+if ($num_needs_license || $num_has_tab || $num_has_cr) {
   my $TODAY = `date +%Y%m%d`;
   my $date = `date`;
 
@@ -46,18 +66,29 @@ if ($num_needs_license) {
   chomp $date;
 
   print "$num_needs_license files need a license.\n";
-  print "$msg\n";
-        # compute date, today
-        my $recipient = "jeffh\@cs.rice.edu,jstewart\@mpi-sws.mpg.de"; # "Pastry Team <freepastry\@cs.rice.edu>";
-        my $whole_msg = <<EOM;
+  print "$msg_license\n";
+  print "$num_has_tab files have tabs.\n";
+  print "$msg_tab\n";   
+  print "$num_has_cr files are dos format.\n";
+  print "$msg_cr\n";   
+  
+  # compute date, today
+  my $recipient = "jeffh\@cs.rice.edu,jstewart\@mpi-sws.mpg.de"; # "Pastry Team <freepastry\@cs.rice.edu>";
+  my $whole_msg = <<EOM;
 From: Automatic Build Server <no-reply\@mpi-sws.mpg.de>
 To: $recipient
-Subject: [pastry-build] $num_needs_license files are missing the license $TODAY
+Subject: [pastry-build] need license:$num_needs_license has tabs:$num_has_tab dos files:$num_has_cr $TODAY
 Date: $date
 
 $num_needs_license files need a license.
+$msg_license
 
-$msg
+$num_has_tab files have tabs.
+$msg_tab   
+
+$num_has_cr files are dos format.
+$msg_cr   
+  
 
 EOM
    open my $fh, "|sendmail $recipient";
