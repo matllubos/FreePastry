2e74ca87e7aab939ceafb45674b022f6ff0fd633
jstewart <jstewart@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2008-06-12 15:24:25 +0000

    Add retries.  A little bit of refactoring to make everything of interest happen on the selector.
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@4227 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/tutorial/lookup/LookupService.java
+++ pastry/src/rice/tutorial/lookup/LookupService.java
@@ -33,7 +33,7 @@ liability, whether in contract, strict liability, or tort (including negligence
 or otherwise) arising in any way out of the use of this software, even if 
 advised of the possibility of such damage.
 
-*******************************************************************************/ 
+ *******************************************************************************/
 package rice.tutorial.lookup;
 
 import java.util.HashMap;
@@ -43,57 +43,70 @@ import rice.environment.logging.Logger;
 import rice.p2p.commonapi.*;
 
 /**
- * The LookupService provides a way to find out what nodes are responsible for
- * a given Id.  It does this by routing a request across the ring, doing a
- * replicaSet lookup at the destination, and sending the result back.  It
- * is perhaps the simplest useful application.
+ * The LookupService provides a way to find out what nodes are responsible for a
+ * given Id. It does this by routing a request across the ring, doing a
+ * replicaSet lookup at the destination, and sending the result back. It is
+ * perhaps the simplest useful application.
  * 
  * @author jstewart
- *
+ * 
  */
 public class LookupService implements Application {
 
   private Endpoint endpoint;
+
   private int seqno;
-  private HashMap<Integer,Continuation<NodeHandleSet,Exception>> pending;
+
+  private HashMap<Integer, Continuation<NodeHandleSet, Exception>> pending;
+
   private long timeout;
+
   private Logger logger;
+
+  private long firstTimeout;
 
   /**
    * Build a LookupService application with a specified timeout
    * 
-   * @param node the local node
-   * @param timeout query timeout in milliseconds
+   * @param node
+   *                the local node
+   * @param timeout
+   *                query timeout in milliseconds
    */
   public LookupService(Node node, long timeout) {
-    this.pending = new HashMap<Integer,Continuation<NodeHandleSet,Exception>>();
+    this.pending = new HashMap<Integer, Continuation<NodeHandleSet, Exception>>();
     this.seqno = 42;
-    
+
     this.endpoint = node.buildEndpoint(this, null);
     endpoint.register();
 
     this.logger = endpoint.getEnvironment().getLogManager().getLogger(getClass(), null);
+    this.firstTimeout = endpoint.getEnvironment().getParameters().getLong("lookup_service.firstTimeout");
 
-    // timeouts should really be adaptive, but that takes more code than this simple app is worth
+    // timeouts should really be adaptive, but that takes more code than this
+    // simple app is worth
     if (timeout > 0) {
       this.timeout = timeout;
     } else {
       // get a default from the parameters file
       this.timeout = endpoint.getEnvironment().getParameters().getLong("lookup_service.timeout");
-      if (this.timeout <= 0)
-        this.timeout = 30000;  // a sane but very large value
+      if (this.timeout <= 0) this.timeout = 30000; // a sane but very large
+                                                    // value
     }
+    
+    if (this.firstTimeout <= 0) this.firstTimeout = this.timeout / 16;
   }
-  
+
   /**
    * Build a LookupService application with the default lookup timeout
    * 
-   * @param node the local node
+   * @param node
+   *                the local node
    */
   public LookupService(Node node) {
     this(node, -1);
   }
-  
+
   /**
    * This Application always forwards its messages
    * 
@@ -106,111 +119,171 @@ public class LookupService implements Application {
   public void deliver(Id id, Message message) {
     if (message instanceof NodeLookupQuery) {
       NodeLookupQuery query = (NodeLookupQuery) message;
-      
-      if (logger.level <= Logger.FINER) logger.log("NodeLookup query received for "+id+" with sequence number "+query.seqno);
 
-      // this is the only interesting line of code in this method, if you can call it interesting
+      if (logger.level <= Logger.FINER) logger.log("NodeLookup query received for " + id + " with sequence number " + query.seqno);
+
+      // this is the only interesting line of code in this method, if you can
+      // call it interesting
       // get our local replica set and ship it back in a response message
       endpoint.route(null, new NodeLookupResponse(endpoint.replicaSet(id, query.numNodes), query.seqno), query.source);
-    
+
     } else if (message instanceof NodeLookupResponse) {
       NodeLookupResponse response = (NodeLookupResponse) message;
-      
+
       if (pending.containsKey(response.seqno)) {
-        
-        if (logger.level <= Logger.FINER) logger.log("NodeLookup response received with sequence number "+response.seqno);
-        
+
+        if (logger.level <= Logger.FINER) logger.log("NodeLookup response received with sequence number " + response.seqno);
+
         pending.remove(response.seqno).receiveResult(response.nodes);
-        
+
       } else {
-        if (logger.level <= Logger.INFO) logger.log("NodeLookup received response for non-existent or expired lookup "+response.seqno);
+        if (logger.level <= Logger.INFO) logger.log("NodeLookup received response for non-existent or expired lookup " + response.seqno);
       }
-    
+
     } else if (message instanceof NodeLookupTimeout) {
       NodeLookupTimeout response = (NodeLookupTimeout) message;
-    
+
       // if it's not in pending, then a message has already been delivered
       if (pending.containsKey(response.seqno)) {
-        
-        if (logger.level <= Logger.FINE) logger.log("NodeLookup timed out with sequence number "+response.seqno);
-        
+
+        if (logger.level <= Logger.FINE) logger.log("NodeLookup timed out with sequence number " + response.seqno);
+
         pending.remove(response.seqno).receiveException(new NodeLookupTimeoutException());
-        
+
       }
-      
+
     } else {
-      if (logger.level <= Logger.WARNING) logger.log("NodeLookup received unexpected message "+message);
+      if (logger.level <= Logger.WARNING) logger.log("NodeLookup received unexpected message " + message);
     }
   }
 
   public void update(NodeHandle handle, boolean joined) {
     // do nothing
-    // this is part of the Application interface, but we don't care about updates
+    // this is part of the Application interface, but we don't care about
+    // updates
   }
 
   /**
-   * Requests a replicaSet from a node across the ring.  This method will return via its 
-   * continuation the set of <tt>num</tt> NodeHandles that are in <tt>id</tt>'s replicaSet.
-   * Calling requestNodeHandles with a <tt>num</tt> of 1 will return the node currently
-   * responsible for Id id.
+   * Requests a replicaSet from a node across the ring. This method will return
+   * via its continuation the set of <tt>num</tt> NodeHandles that are in
+   * <tt>id</tt>'s replicaSet. Calling requestNodeHandles with a <tt>num</tt>
+   * of 1 will return the node currently responsible for Id id.
    * 
-   * If there is a network problem or the Id of interest is not currently owned by any node
-   * (for example due to churn in the overlay), the continuation will receive a 
-   * NodeLookupTimeoutException.
+   * If there is a network problem or the Id of interest is not currently owned
+   * by any node (for example due to churn in the overlay), the continuation
+   * will receive a NodeLookupTimeoutException.
    * 
-   * @param id the Id of interest
-   * @param num the number of nodes to return
-   * @param cont the continuation to return the result to
+   * @param id
+   *                the Id of interest
+   * @param num
+   *                the number of nodes to return
+   * @param cont
+   *                the continuation to return the result to
    */
-  public void requestNodeHandles(Id id, int num, Continuation<NodeHandleSet,Exception> cont) {
-    int seq = seqno++;
-    pending.put(seq, cont);
-    
-    if (logger.level <= Logger.FINER) logger.log("NodeLookup being sent to id  "+id+" with sequence number "+seq);
-    
-    endpoint.route(id, new NodeLookupQuery(endpoint.getLocalNodeHandle(), num, seq), null);
-    endpoint.scheduleMessage(new NodeLookupTimeout(seq), timeout);
+  public void requestNodeHandles(final Id id, final int num, final Continuation<NodeHandleSet, Exception> cont) {
+    endpoint.getEnvironment().getSelectorManager().invoke(new Runnable() {
+      public void run() {
+        sendMessageWithRetries(id, num, cont);
+      }
+    });
   }
-  
-  /**
-   * Find the primary replica of a key.  This is the node with the Id closest to the key in pastry.
-   * This method will return via its continuation the NodeHandle that is closest to <tt>id</tt>.
-   *
-   * If there is a network problem or the Id of interest is not currently owned by any node
-   * (for example due to churn in the overlay), the continuation will receive a 
-   * NodeLookupTimeoutException.
-   * 
-   * @param id the Id of interest
-   * @param cont the continuation to return the result to
-   */
-  public void requestNodeHandle(Id id, final Continuation<NodeHandle,Exception> cont) {
-    int seq = seqno++;
-    pending.put(seq, new Continuation<NodeHandleSet, Exception>() {
+
+  protected void sendMessageWithRetries(final Id id, final int num, final Continuation<NodeHandleSet, Exception> cont) {
+    final int seq = seqno++;
+    sendMessage(seq, id, num, new Continuation<NodeHandleSet, Exception>() {
+      long t = firstTimeout; // how long our current timeout is
+
+      long cum = 0; // how long we've been waiting total
+
       public void receiveException(Exception exception) {
-        cont.receiveException(exception);
+        if (exception instanceof NodeLookupTimeoutException) {
+          cum += t;
+          // have we hit our final timeout?
+          if (cum < timeout) {
+            t *= 2;
+            // would we surpass our final timeout wit the new t?
+            if (cum + t < timeout) {
+              // retransmit: same sequence number, same continuation, new timeout
+              sendMessage(seq, id, num, this, t);
+            } else {
+              // for some values of firstTimeout,timeout this will retransmit
+              // the last time with stupidly low timeouts
+              sendMessage(seq, id, num, this, timeout - cum);
+            }
+          } else {
+            cont.receiveException(exception);
+          }
+        } else {
+          cont.receiveException(exception);
+        }
       }
 
       public void receiveResult(NodeHandleSet result) {
-        if (result.size() != 1) {
-          // this really shouldn't happen
-          receiveException(new IndexOutOfBoundsException("Expected 1 result, got "+result.size()));
-        } else {
-          cont.receiveResult(result.getHandle(0));
-        }
+        cont.receiveResult(result);
       }
-    });
-    
-    if (logger.level <= Logger.FINER) logger.log("NodeLookup being sent to id  "+id+" with sequence number "+seq);
-    
-    endpoint.route(id, new NodeLookupQuery(endpoint.getLocalNodeHandle(), 1, seq), null);
+    }, firstTimeout);
+  }
+
+  /**
+   * This does the internal processing for requestNodeHandle(s). It adds the
+   * continuation to the pending table, updates the sequence number, schedules
+   * this timeout, and finally sends the actual message.
+   * 
+   * @param id
+   *                the Id of interest
+   * @param num
+   *                the number of nodes to return
+   * @param cont
+   *                the continuation to return the result to
+   */
+  protected void sendMessage(int seq, Id id, int num, Continuation<NodeHandleSet, Exception> cont, long timeout) {
+    pending.put(seq, cont);
+
+    if (logger.level <= Logger.FINER) logger.log("NodeLookup being sent to id  " + id + " with sequence number " + seq);
+
+    endpoint.route(id, new NodeLookupQuery(endpoint.getLocalNodeHandle(), num, seq), null);
     endpoint.scheduleMessage(new NodeLookupTimeout(seq), timeout);
   }
-  
+
+  /**
+   * Find the primary replica of a key. This is the node with the Id closest to
+   * the key in pastry. This method will return via its continuation the
+   * NodeHandle that is closest to <tt>id</tt>.
+   * 
+   * If there is a network problem or the Id of interest is not currently owned
+   * by any node (for example due to churn in the overlay), the continuation
+   * will receive a NodeLookupTimeoutException.
+   * 
+   * @param id
+   *                the Id of interest
+   * @param cont
+   *                the continuation to return the result to
+   */
+  public void requestNodeHandle(final Id id, final Continuation<NodeHandle, Exception> cont) {
+    endpoint.getEnvironment().getSelectorManager().invoke(new Runnable() {
+      public void run() {
+        sendMessageWithRetries(id, 1, new Continuation<NodeHandleSet, Exception>() {
+          public void receiveException(Exception exception) {
+            cont.receiveException(exception);
+          }
+
+          public void receiveResult(NodeHandleSet result) {
+            if (result.size() != 1) {
+              // this really shouldn't happen
+              receiveException(new IndexOutOfBoundsException("Expected 1 result, got " + result.size()));
+            } else {
+              cont.receiveResult(result.getHandle(0));
+            }
+          }
+        });
+      }
+    });
+  }
+
   /**
    * This is a message used internally for implementing timeouts
    * 
    * @author jstewart
-   *
    */
   public static class NodeLookupTimeout implements Message {
     private static final long serialVersionUID = 1648981192042898092L;
@@ -230,7 +303,6 @@ public class LookupService implements Application {
    * The query message
    * 
    * @author jstewart
-   *
    */
   public static class NodeLookupQuery implements Message {
     private static final long serialVersionUID = -4882776401593706141L;
@@ -248,12 +320,11 @@ public class LookupService implements Application {
       return 0;
     }
   }
-  
+
   /**
    * The response message
    * 
    * @author jstewart
-   *
    */
   public static class NodeLookupResponse implements Message {
     private static final long serialVersionUID = -3200682143184682743L;
@@ -271,11 +342,10 @@ public class LookupService implements Application {
   }
 
   /**
-   * Returned by continuations handed to requestNodeHandles when a request
-   * times out.
+   * Returned by continuations handed to requestNodeHandles when a request times
+   * out.
    * 
    * @author jstewart
-   *
    */
   public static class NodeLookupTimeoutException extends Exception {
     private static final long serialVersionUID = -9138111846775601203L;
