3c7e2ae967f6c056a8c3d28a9f47987693f7efda
jstewart <jstewart@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2008-06-20 09:39:00 +0000

    fix license again.  stupid white space.
    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@4229 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/tutorial/lookup/LookupService.java
+++ pastry/src/rice/tutorial/lookup/LookupService.java
@@ -33,7 +33,7 @@ liability, whether in contract, strict liability, or tort (including negligence
 or otherwise) arising in any way out of the use of this software, even if 
 advised of the possibility of such damage.
 
-*******************************************************************************/
+*******************************************************************************/ 
 package rice.tutorial.lookup;
 
 import java.util.HashMap;
