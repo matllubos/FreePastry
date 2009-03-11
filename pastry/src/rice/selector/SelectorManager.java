9f5164bb42e0c09416a557e4bcf3a9c42d8d2cfb
jeffh <jeffh@c21dacaf-8602-0410-aacd-8bc41df2cbe7>
2009-03-11 15:54:34 +0000

    git-svn-id: https://svn.mpi-sws.org/DS/freepastry/trunk@4675 c21dacaf-8602-0410-aacd-8bc41df2cbe7

--- pastry/src/rice/selector/SelectorManager.java
+++ pastry/src/rice/selector/SelectorManager.java
@@ -391,6 +391,7 @@ public class SelectorManager extends Thread implements Timer, Destructable {
     }
 
     for (int i = 0; i < keys.length; i++) {
+//      System.out.println("handling key "+keys[i].attachment());
       selector.selectedKeys().remove(keys[i]);      
 
       synchronized (keys[i]) {
